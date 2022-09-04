9e2e7d559305e14dce42193de4acc11e04d9b3b0
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2022-09-04 16:35:51 +0200

    move to ClientProxy

--- src/main/java/com/gtnewhorizon/gtnhlib/ClientProxy.java
+++ src/main/java/com/gtnewhorizon/gtnhlib/ClientProxy.java
@@ -1,15 +1,14 @@
 package com.gtnewhorizon.gtnhlib;
 
-import cpw.mods.fml.common.event.FMLInitializationEvent;
-import cpw.mods.fml.common.event.FMLPostInitializationEvent;
-import cpw.mods.fml.common.event.FMLPreInitializationEvent;
-import cpw.mods.fml.common.event.FMLServerAboutToStartEvent;
-import cpw.mods.fml.common.event.FMLServerStartedEvent;
-import cpw.mods.fml.common.event.FMLServerStartingEvent;
-import cpw.mods.fml.common.event.FMLServerStoppedEvent;
-import cpw.mods.fml.common.event.FMLServerStoppingEvent;
+import cpw.mods.fml.common.event.*;
+import net.minecraft.client.Minecraft;
+import net.minecraft.util.ChatComponentText;
+import net.minecraft.util.EnumChatFormatting;
+import net.minecraft.util.IChatComponent;
 
 public class ClientProxy extends CommonProxy {
+
+    private final Minecraft mc = Minecraft.getMinecraft();
 
     public void preInit(FMLPreInitializationEvent event) {
         super.preInit(event);
@@ -41,5 +40,44 @@ public class ClientProxy extends CommonProxy {
 
     public void serverStopped(FMLServerStoppedEvent event) {
         super.serverStopped(event);
+    }
+
+    public void addDebugToChat(String message) {
+        addDebugToChat(new ChatComponentText(message));
+    }
+
+    public void addDebugToChat(IChatComponent componentText) {
+        addMessageToChat(new ChatComponentText(EnumChatFormatting.YELLOW + "[Debug]: ").appendSibling(componentText));
+    }
+
+    public void addInfoToChat(String message) {
+        addInfoToChat(new ChatComponentText(message));
+    }
+
+    public void addInfoToChat(IChatComponent componentText) {
+        addMessageToChat(new ChatComponentText(EnumChatFormatting.GREEN + "[Info]: ").appendSibling(componentText));
+    }
+
+    public void addWarnToChat(String message) {
+        addWarnToChat(new ChatComponentText(message));
+    }
+
+    public void addWarnToChat(IChatComponent componentText) {
+        addMessageToChat(
+                new ChatComponentText(EnumChatFormatting.LIGHT_PURPLE + "[Warn]: ").appendSibling(componentText));
+    }
+
+    public void addErrorToChat(String message) {
+        addErrorToChat(new ChatComponentText(message));
+    }
+
+    public void addErrorToChat(IChatComponent componentText) {
+        addMessageToChat(new ChatComponentText(EnumChatFormatting.RED + "[Error]: ").appendSibling(componentText));
+    }
+
+    public void addMessageToChat(IChatComponent componentText) {
+        if (mc.theWorld != null && mc.thePlayer != null) {
+            mc.thePlayer.addChatMessage(componentText);
+        }
     }
 }
--- src/main/java/com/gtnewhorizon/gtnhlib/GTNHLib.java
+++ src/main/java/com/gtnewhorizon/gtnhlib/GTNHLib.java
@@ -3,17 +3,12 @@ package com.gtnewhorizon.gtnhlib;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.*;
-import net.minecraft.client.Minecraft;
-import net.minecraft.util.ChatComponentText;
-import net.minecraft.util.EnumChatFormatting;
-import net.minecraft.util.IChatComponent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 @Mod(modid = Tags.MODID, version = Tags.VERSION, name = Tags.MODNAME, acceptedMinecraftVersions = "[1.7.10]")
 public class GTNHLib {
 
-    private static final Minecraft mc = Minecraft.getMinecraft();
     private static final Logger LOG = LogManager.getLogger(Tags.MODID);
 
     @SidedProxy(clientSide = Tags.GROUPNAME + ".ClientProxy", serverSide = Tags.GROUPNAME + ".CommonProxy")
@@ -73,44 +68,5 @@ public class GTNHLib {
 
     public static void error(String message) {
         LOG.error(message);
-    }
-
-    public static void addDebugToChat(String message) {
-        addDebugToChat(new ChatComponentText(message));
-    }
-
-    public static void addDebugToChat(IChatComponent componentText) {
-        addMessageToChat(new ChatComponentText(EnumChatFormatting.YELLOW + "[Debug]: ").appendSibling(componentText));
-    }
-
-    public static void addInfoToChat(String message) {
-        addInfoToChat(new ChatComponentText(message));
-    }
-
-    public static void addInfoToChat(IChatComponent componentText) {
-        addMessageToChat(new ChatComponentText(EnumChatFormatting.GREEN + "[Info]: ").appendSibling(componentText));
-    }
-
-    public static void addWarnToChat(String message) {
-        addWarnToChat(new ChatComponentText(message));
-    }
-
-    public static void addWarnToChat(IChatComponent componentText) {
-        addMessageToChat(
-                new ChatComponentText(EnumChatFormatting.LIGHT_PURPLE + "[Warn]: ").appendSibling(componentText));
-    }
-
-    public static void addErrorToChat(String message) {
-        addErrorToChat(new ChatComponentText(message));
-    }
-
-    public static void addErrorToChat(IChatComponent componentText) {
-        addMessageToChat(new ChatComponentText(EnumChatFormatting.RED + "[Error]: ").appendSibling(componentText));
-    }
-
-    public static void addMessageToChat(IChatComponent componentText) {
-        if (mc.theWorld != null && mc.thePlayer != null) {
-            mc.thePlayer.addChatMessage(componentText);
-        }
     }
 }
