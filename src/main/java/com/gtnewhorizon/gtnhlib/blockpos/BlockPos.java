d172eae4a1516cbf87d89afbc90307496c570da5
miozune <miozune@gmail.com>
2024-08-13 20:01:18 +0900

    Backport BlockPos#getAllInBox

--- src/main/java/com/gtnewhorizon/gtnhlib/blockpos/BlockPos.java
+++ src/main/java/com/gtnewhorizon/gtnhlib/blockpos/BlockPos.java
@@ -1,10 +1,13 @@
 package com.gtnewhorizon.gtnhlib.blockpos;
 
+import java.util.Iterator;
+
 import net.minecraft.world.ChunkPosition;
 import net.minecraftforge.common.util.ForgeDirection;
 
 import org.joml.Vector3i;
 
+import com.google.common.collect.AbstractIterator;
 import com.gtnewhorizon.gtnhlib.util.CoordinatePacker;
 
 public class BlockPos extends Vector3i implements IMutableBlockPos {
@@ -66,5 +69,50 @@ public class BlockPos extends Vector3i implements IMutableBlockPos {
     public BlockPos set(long packedPos) {
         CoordinatePacker.unpack(packedPos, this);
         return this;
+    }
+
+    public static Iterable<BlockPos> getAllInBox(BlockPos from, BlockPos to) {
+        return getAllInBox(
+                Math.min(from.getX(), to.getX()),
+                Math.min(from.getY(), to.getY()),
+                Math.min(from.getZ(), to.getZ()),
+                Math.max(from.getX(), to.getX()),
+                Math.max(from.getY(), to.getY()),
+                Math.max(from.getZ(), to.getZ()));
+    }
+
+    public static Iterable<BlockPos> getAllInBox(final int x1, final int y1, final int z1, final int x2, final int y2,
+            final int z2) {
+        return new Iterable<>() {
+
+            public Iterator<BlockPos> iterator() {
+                return new AbstractIterator<>() {
+
+                    private BlockPos pos;
+
+                    protected BlockPos computeNext() {
+                        if (this.pos == null) {
+                            this.pos = new BlockPos(x1, y1, z1);
+                            return this.pos;
+                        } else if (this.pos.x == x2 && this.pos.y == y2 && this.pos.z == z2) {
+                            return this.endOfData();
+                        } else {
+                            if (this.pos.x < x2) {
+                                ++this.pos.x;
+                            } else if (this.pos.y < y2) {
+                                this.pos.x = x1;
+                                ++this.pos.y;
+                            } else if (this.pos.z < z2) {
+                                this.pos.x = x1;
+                                this.pos.y = y1;
+                                ++this.pos.z;
+                            }
+
+                            return this.pos;
+                        }
+                    }
+                };
+            }
+        };
     }
 }
