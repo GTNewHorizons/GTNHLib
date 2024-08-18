aac0f69288a995f0ab650c3b6e605e1fe59f1631
Caedis <Caedis@users.noreply.github.com>
2024-08-18 11:59:50 -0500

    spotless

--- src/main/java/com/gtnewhorizon/gtnhlib/mixins/Mixins.java
+++ src/main/java/com/gtnewhorizon/gtnhlib/mixins/Mixins.java
@@ -15,9 +15,11 @@ import cpw.mods.fml.relauncher.FMLLaunchHandler;
 public enum Mixins {
 
     TESSELLATOR(new Builder("Sodium").addTargetedMod(TargetedMod.VANILLA).setSide(Side.CLIENT).setPhase(Phase.EARLY)
-            .setApplyIf(() -> true).addExcludedMod(TargetedMod.OPTIFINE).addExcludedMod(TargetedMod.FASTCRAFT).addMixinClasses("MixinTessellator")),
+            .setApplyIf(() -> true).addExcludedMod(TargetedMod.OPTIFINE).addExcludedMod(TargetedMod.FASTCRAFT)
+            .addMixinClasses("MixinTessellator")),
     WAVEFRONT_VBO(new Builder("WavefrontObject").addTargetedMod(TargetedMod.VANILLA).setSide(Side.CLIENT)
-            .setPhase(Phase.EARLY).setApplyIf(() -> true).addExcludedMod(TargetedMod.OPTIFINE).addExcludedMod(TargetedMod.FASTCRAFT).addMixinClasses("MixinWavefrontObject")),;
+            .setPhase(Phase.EARLY).setApplyIf(() -> true).addExcludedMod(TargetedMod.OPTIFINE)
+            .addExcludedMod(TargetedMod.FASTCRAFT).addMixinClasses("MixinWavefrontObject")),;
 
     private final List<String> mixinClasses;
     private final Supplier<Boolean> applyIf;
