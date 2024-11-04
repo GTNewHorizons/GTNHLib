7409459b81bcafbc1073e17f7c24b1923bab0cc8
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2024-11-04 03:55:15 -0500

    Shadow source jars too

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,3 +9,9 @@ tasks.processResources {
         expand("version" to project.version.toString())
     }
 }
+
+// Shadow source jars too
+val shadowSources = configurations.getByName("shadowSources")
+tasks.sourcesJar {
+    from(shadowSources.map { zipTree(it) }) // idr what the operator here is, something that applies zipTree to all elements
+}
