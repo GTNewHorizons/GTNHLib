bb1602fff07dd4f29444772db38cacf0d64a53a6
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2024-11-11 02:30:19 -0500

    spotless

--- src/main/java/com/gtnewhorizon/gtnhlib/compat/Mods.java
+++ src/main/java/com/gtnewhorizon/gtnhlib/compat/Mods.java
@@ -3,5 +3,6 @@ package com.gtnewhorizon.gtnhlib.compat;
 import cpw.mods.fml.common.Loader;
 
 public class Mods {
+
     public static final boolean FALSETWEAKS = Loader.isModLoaded("falsetweaks");
 }
