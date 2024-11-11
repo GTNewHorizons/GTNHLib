623d0ce7fa51adc443ee63216b16169f909cc68d
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2024-11-11 12:06:19 -0500

    Add `getThreadTesselator` to the API

--- src/main/java/com/gtnewhorizon/gtnhlib/api/CapturingTesselator.java
+++ src/main/java/com/gtnewhorizon/gtnhlib/api/CapturingTesselator.java
@@ -1,10 +1,23 @@
 package com.gtnewhorizon.gtnhlib.api;
 
 import com.gtnewhorizon.gtnhlib.client.renderer.TessellatorManager;
+import net.minecraft.client.renderer.Tessellator;
 
-public class CapturingTesselator {
-
-    public static boolean isCapturing() {
+@SuppressWarnings("unused")
+public interface CapturingTesselator {
+    /**
+     * @return True if this thread is capturing quads, false otherwise
+     */
+    static boolean isCapturing() {
         return TessellatorManager.isCurrentlyCapturing();
+    }
+
+    /**
+     * @throws IllegalStateException If the thread is not capturing and is not the main one.
+     * @return The CapturingTesselator for this thread if capturing, or else {@link Tessellator#instance} if on the main
+     * one.
+     */
+    static Tessellator getThreadTesselator() {
+        return TessellatorManager.get();
     }
 }
