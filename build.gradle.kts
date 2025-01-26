6cceac72b7570a0997e6f1a7bb7dfbdb98db812a
Raven Szewczyk <git@eigenraven.me>
2025-01-26 14:54:00 +0000

    Update buildscript, dependencies, make the jar multi-release in preparation for extra modern java features

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,4 +14,25 @@ tasks.processResources {
 val shadowSources = configurations.getByName("shadowSources")
 tasks.sourcesJar {
     from(shadowSources.map { zipTree(it) })
+    exclude("META-INF/versions/9/module-info.class")
+    exclude("META-INF/versions/9/module-info.java")
+}
+
+tasks.shadowJar {
+    exclude("META-INF/versions/9/module-info.class")
+}
+
+tasks.test {
+    useJUnitPlatform()
+    testLogging {
+        events("passed", "skipped", "failed")
+    }
+}
+
+for (jarTask in listOf(tasks.jar, tasks.shadowJar)) {
+    jarTask.configure {
+        manifest {
+            attributes("Multi-Release" to true)
+        }
+    }
 }
