8cf02fafab3a6814d3ed8f838daddab9407740d9
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2025-02-04 22:25:43 -0500

    Spotless

--- src/main/java/com/gtnewhorizon/gtnhlib/asm/ClassConstantPoolParser.java
+++ src/main/java/com/gtnewhorizon/gtnhlib/asm/ClassConstantPoolParser.java
@@ -32,6 +32,7 @@ import java.util.Arrays;
 public class ClassConstantPoolParser {
 
     enum ConstantTypes {
+
         INVALID, // 0 unused, if that ever changes shift this to the next unused index
         UTF8,
         // 2 unused
@@ -53,12 +54,16 @@ public class ClassConstantPoolParser {
         MODULE,
         PACKAGE;
 
-        static final ConstantTypes[] MAP = {
-            INVALID,
-            UTF8, INVALID,
-            INT, FLOAT, LONG, DOUBLE, CLASS_REF, STR_REF, FIELD, METH_REF, IMETH_REF, NAME_TYPE, INVALID, INVALID,
-            METH_HANDLE, METH_TYPE, DYNAMIC, INVOKE_DYNAMIC, MODULE, PACKAGE
-        };
+        // spotless:off
+        // Indices in this table directly map to the values of these constants - disable spotless to make it easier to
+        // see this
+        static final ConstantTypes[] MAP = {      INVALID,
+            UTF8,    INVALID,        INT,         FLOAT,
+            LONG,    DOUBLE,         CLASS_REF,   STR_REF,
+            FIELD,   METH_REF,       IMETH_REF,   NAME_TYPE,
+            INVALID, INVALID,        METH_HANDLE, METH_TYPE,
+            DYNAMIC, INVOKE_DYNAMIC, MODULE,      PACKAGE };
+        //spotless:on
 
         static ConstantTypes toType(byte code) {
             var ret = MAP[Byte.toUnsignedInt(code)];
@@ -135,7 +140,10 @@ public class ClassConstantPoolParser {
                     }
                 }
                 case INT, FLOAT, FIELD, METH_REF, IMETH_REF, NAME_TYPE, DYNAMIC, INVOKE_DYNAMIC -> size = 5;
-                case LONG, DOUBLE -> { size = 9; ++i; }
+                case LONG, DOUBLE -> {
+                    size = 9;
+                    ++i;
+                }
                 case METH_HANDLE -> size = 4;
                 case CLASS_REF, STR_REF, METH_TYPE, MODULE, PACKAGE -> size = 3;
             }
