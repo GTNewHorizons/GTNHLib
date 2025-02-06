dad19a01a5073855de41efc21bdd1f528e62e37e
Raven Szewczyk <git@eigenraven.me>
2025-02-06 17:08:13 +0000

    Spotless

--- src/main/java/com/gtnewhorizon/gtnhlib/capability/CapabilityUtil.java
+++ src/main/java/com/gtnewhorizon/gtnhlib/capability/CapabilityUtil.java
@@ -5,6 +5,7 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraftforge.common.util.ForgeDirection;
+
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
