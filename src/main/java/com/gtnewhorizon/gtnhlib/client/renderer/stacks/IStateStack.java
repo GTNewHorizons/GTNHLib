1b7d8895baabdf2d4cba82a0315db00609033221
Jason Mitchell <mitchej@gmail.com>
2025-03-11 13:00:39 -0700

    Updates for IStateStack * Add javadoc strings and add a clear() method

--- src/main/java/com/gtnewhorizon/gtnhlib/client/renderer/stacks/IStateStack.java
+++ src/main/java/com/gtnewhorizon/gtnhlib/client/renderer/stacks/IStateStack.java
@@ -1,11 +1,33 @@
 package com.gtnewhorizon.gtnhlib.client.renderer.stacks;
 
 public interface IStateStack<T> {
-
+    /**
+     * Push the current state onto the stack.
+     * @return This state stack for chaining
+     */
     T push();
 
+    /**
+     * Pop a state from the stack.
+     * @return This state stack for chaining
+     */
     T pop();
 
+    /**
+     * Check if the stack is empty.
+     * @return true if the stack is empty
+     */
     boolean isEmpty();
 
+    /**
+     * Clear the stack and reset to default state.
+     * @return This state stack for chaining
+     */
+    @SuppressWarnings("unchecked")
+    default T clear() {
+        while (!isEmpty()) {
+            pop();
+        }
+        return (T) this;
+    }
 }
