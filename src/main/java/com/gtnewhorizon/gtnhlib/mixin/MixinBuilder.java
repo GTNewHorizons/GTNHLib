2df84adbf7a695944118a182ed941165b7b0950d
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2025-06-30 00:53:20 +0200

    make specifying TargetMod optional

--- src/main/java/com/gtnewhorizon/gtnhlib/mixin/MixinBuilder.java
+++ src/main/java/com/gtnewhorizon/gtnhlib/mixin/MixinBuilder.java
@@ -11,25 +11,26 @@ import javax.annotation.Nullable;
 
 import cpw.mods.fml.relauncher.FMLLaunchHandler;
 
+@SuppressWarnings("unused")
 public class MixinBuilder {
 
     private @Nullable List<String> commonMixins;
     private @Nullable List<String> clientMixins;
     private @Nullable List<String> serverMixins;
-    private final List<ITargetedMod> targetedMods = new ArrayList<>(1);
+    private @Nullable List<ITargetedMod> requiredMods;
     private @Nullable List<ITargetedMod> excludedMods;
     private Phase phase;
     private Supplier<Boolean> applyIf = () -> true;
 
     public MixinBuilder() {}
 
-    public MixinBuilder(@SuppressWarnings("unused") String description) {}
+    public MixinBuilder(String description) {}
 
     /**
      * Add mixins that should be loaded both on the client and server
      */
     public MixinBuilder addCommonMixins(@Nonnull String... mixins) {
-        if (commonMixins == null) commonMixins = new ArrayList<>();
+        if (commonMixins == null) commonMixins = new ArrayList<>(4);
         Collections.addAll(commonMixins, mixins);
         return this;
     }
@@ -38,7 +39,7 @@ public class MixinBuilder {
      * Add mixins that should only be loaded on the client
      */
     public MixinBuilder addClientMixins(@Nonnull String... mixins) {
-        if (clientMixins == null) clientMixins = new ArrayList<>();
+        if (clientMixins == null) clientMixins = new ArrayList<>(4);
         Collections.addAll(clientMixins, mixins);
         return this;
     }
@@ -47,7 +48,7 @@ public class MixinBuilder {
      * Add mixins that should only be loaded on the dedicated server
      */
     public MixinBuilder addServerMixins(@Nonnull String... mixins) {
-        if (serverMixins == null) serverMixins = new ArrayList<>();
+        if (serverMixins == null) serverMixins = new ArrayList<>(4);
         Collections.addAll(serverMixins, mixins);
         return this;
     }
@@ -62,13 +63,20 @@ public class MixinBuilder {
         return this;
     }
 
-    public MixinBuilder addTargetedMod(ITargetedMod mod) {
-        targetedMods.add(mod);
+    /**
+     * Specify mods that need to be present for this mixin to load
+     */
+    public MixinBuilder addRequiredMod(ITargetedMod mod) {
+        if (requiredMods == null) requiredMods = new ArrayList<>(2);
+        requiredMods.add(mod);
         return this;
     }
 
+    /**
+     * Specify mods that disable this mixin if they are present
+     */
     public MixinBuilder addExcludedMod(ITargetedMod mod) {
-        if (excludedMods == null) excludedMods = new ArrayList<>();
+        if (excludedMods == null) excludedMods = new ArrayList<>(2);
         excludedMods.add(mod);
         return this;
     }
@@ -80,9 +88,6 @@ public class MixinBuilder {
         if (serverMixins != null) count += serverMixins.size();
         if (count == 0) {
             throw new RuntimeException("No mixin class registered for IMixins : " + entry.name());
-        }
-        if (targetedMods.isEmpty()) {
-            throw new RuntimeException("No targeted mods registered for IMixins : " + entry.name());
         }
         if (phase == null) {
             throw new RuntimeException("No Phase specified for IMixins : " + entry.name());
@@ -110,13 +115,14 @@ public class MixinBuilder {
 
     private boolean shouldLoadMixin(Set<String> loadedCoreMods, Set<String> loadedMods) {
         return applyIf.get() && noExcludedModsPresent(loadedCoreMods, loadedMods)
-                && allTargetedModsPresent(loadedCoreMods, loadedMods);
+                && allRequiredModsPresent(loadedCoreMods, loadedMods);
     }
 
-    private boolean allTargetedModsPresent(Set<String> loadedCoreMods, Set<String> loadedMods) {
+    private boolean allRequiredModsPresent(Set<String> loadedCoreMods, Set<String> loadedMods) {
+        if (requiredMods == null) return true;
         // noinspection ForLoopReplaceableByForEach
-        for (int i = 0; i < targetedMods.size(); i++) {
-            ITargetedMod target = targetedMods.get(i);
+        for (int i = 0; i < requiredMods.size(); i++) {
+            ITargetedMod target = requiredMods.get(i);
             if (target == TargetedMod.VANILLA) continue;
             // Check coremod first
             if (!loadedCoreMods.isEmpty() && target.getCoreModClass() != null
