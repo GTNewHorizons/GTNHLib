8aeb9d140a62d59778a9ac687a2a50de245454ff
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2025-08-22 07:16:28 -0400

    Use ObjectList in signature

--- src/main/java/com/gtnewhorizon/gtnhlib/json/BlockStateDef.java
+++ src/main/java/com/gtnewhorizon/gtnhlib/json/BlockStateDef.java
@@ -1,6 +1,7 @@
 package com.gtnewhorizon.gtnhlib.json;
 
 import com.github.bsideup.jabel.Desugar;
+import com.gtnewhorizon.gtnhlib.block.BlockState;
 import com.gtnewhorizon.gtnhlib.client.model.Variant;
 import it.unimi.dsi.fastutil.objects.Object2ObjectMap;
 import it.unimi.dsi.fastutil.objects.Object2ObjectMaps;
@@ -9,12 +10,14 @@ import it.unimi.dsi.fastutil.objects.ObjectList;
 import it.unimi.dsi.fastutil.objects.ObjectLists;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+import java.util.Random;
 
 public class BlockStateDef {
-    private final Object2ObjectMap<StateMatch, List<Variant>> variants;
+    private final Object2ObjectMap<StateMatch, ObjectList<Variant>> variants;
     private final ObjectList<Case> multipart;
 
-    BlockStateDef(Object2ObjectMap<StateMatch, List<Variant>> variants) {
+    BlockStateDef(Object2ObjectMap<StateMatch, ObjectList<Variant>> variants) {
         this.variants = Object2ObjectMaps.unmodifiable(variants);
         multipart = null;
     }
@@ -24,9 +27,45 @@ public class BlockStateDef {
         this.multipart = ObjectLists.unmodifiable(multipart);
     }
 
+    public Variant getModelLocation(Map<String, String> properties, Random rand) {
+        if (variants == null) return getModelLocationMultipart(properties);
+        return getModelLocationVariants(properties, rand);
+    }
+
+    private Variant getModelLocationVariants(Map<String, String> properties, Random rand) {
+        final var iter = Object2ObjectMaps.fastIterator(variants);
+        while (iter.hasNext()) {
+            final var e = iter.next();
+            final var match = e.getKey();
+
+            if (match.matches(properties)) return selectOne(e.getValue(), rand);
+        }
+
+        return null;
+    }
+
+    private Variant getModelLocationMultipart(Map<String, String> properties) {
+    }
+
+    private Variant selectOne(ObjectList<Variant> variants, Random rand) {
+        var weight = 0;
+        for (var v : variants) {
+            weight += v.weight;
+        }
+
+        final var selector = rand.nextInt(weight);
+        weight = 0;
+        for (var v : variants) {
+            if (selector <= weight) return v;
+            weight += v.weight;
+        }
+
+        throw new IllegalStateException("Randomly selected beyond the list!");
+    }
+
     static class StateMatch {
         private final boolean matchAll;
-        private final Map<String, String> states;
+        private final Object2ObjectMap<String, String> states;
 
         StateMatch(String s) {
             if (s.isEmpty()) {
@@ -44,6 +83,18 @@ public class BlockStateDef {
 
                 states.put(c.substring(0, eidx), c.substring(eidx));
             }
+        }
+
+        boolean matches(Map<String, String> properties) {
+            if (matchAll) return true;
+
+            final var iter = Object2ObjectMaps.fastIterator(states);
+            while (iter.hasNext()) {
+                final var e = iter.next();
+                if (!Objects.equals(properties.get(e.getKey()), e.getValue())) return false;
+            }
+
+            return true;
         }
     }
 
