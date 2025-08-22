404679fdf9bbfcec005049a989b1cfe563a398da
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2025-08-22 11:05:48 -0400

    Create MultipartModel.java

---
+++ src/main/java/com/gtnewhorizon/gtnhlib/client/model/MultipartModel.java
@@ -0,0 +1,10 @@
+package com.gtnewhorizon.gtnhlib.client.model;
+
+import com.gtnewhorizon.gtnhlib.client.renderer.quad.BakedModel;
+
+public class MultipartModel implements UnbakedModel {
+    @Override
+    public BakedModel bake() {
+        return null;
+    }
+}
