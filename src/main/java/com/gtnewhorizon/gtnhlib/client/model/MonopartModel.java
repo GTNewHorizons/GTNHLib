af1b124dfd6fd01e9d4bba8dcef9091abb870ebf
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2025-08-23 12:05:38 -0400

    add bake data to unbaked model call

--- src/main/java/com/gtnewhorizon/gtnhlib/client/model/MonopartModel.java
+++ src/main/java/com/gtnewhorizon/gtnhlib/client/model/MonopartModel.java
@@ -1,7 +1,6 @@
 package com.gtnewhorizon.gtnhlib.client.model;
 
-public class MultipartModel implements UnbakedModel {
-
+public class MonopartModel implements UnbakedModel {
     @Override
     public BakedModel bake(BakeData data) {
         return null;
