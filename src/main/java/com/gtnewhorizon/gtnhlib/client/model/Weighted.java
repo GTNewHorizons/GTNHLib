8bf7854ebffd65bc7b3053185b8fc1a1036af0e5
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2025-08-24 07:06:30 -0400

    Make Weighted generic

---
+++ src/main/java/com/gtnewhorizon/gtnhlib/client/model/Weighted.java
@@ -0,0 +1,24 @@
+package com.gtnewhorizon.gtnhlib.client.model;
+
+import com.github.bsideup.jabel.Desugar;
+import it.unimi.dsi.fastutil.objects.ObjectList;
+import java.util.Random;
+
+@Desugar
+public record Weighted<T>(T thing, int weight) {
+    private static <T> T selectOne(ObjectList<Weighted<T>> heavyThings, Random rand) {
+        var weight = 0;
+        for (var v : heavyThings) {
+            weight += v.weight();
+        }
+
+        final var selector = rand.nextInt(weight);
+        weight = 0;
+        for (var v : heavyThings) {
+            if (selector <= weight) return v.thing();
+            weight += v.weight();
+        }
+
+        throw new IllegalStateException("Randomly selected beyond the list!");
+    }
+}
