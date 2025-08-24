055d79fbc780ba962fb69e0cd7d4add56a07f79a
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2025-08-24 07:16:43 -0400

    Implement monopart models

--- src/main/java/com/gtnewhorizon/gtnhlib/client/model/baked/MonopartModel.java
+++ src/main/java/com/gtnewhorizon/gtnhlib/client/model/baked/MonopartModel.java
@@ -1,4 +1,20 @@
 package com.gtnewhorizon.gtnhlib.client.model.baked;
 
-public class MonopartModel {
+import com.gtnewhorizon.gtnhlib.client.model.BakedModel;
+import com.gtnewhorizon.gtnhlib.client.model.Weighted;
+import com.gtnewhorizon.gtnhlib.client.renderer.quad.QuadView;
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.function.Supplier;
+import net.minecraft.block.Block;
+import net.minecraft.world.IBlockAccess;
+import net.minecraftforge.common.util.ForgeDirection;
+import org.jetbrains.annotations.Nullable;
+
+public record MonopartModel(ObjectArrayList<Weighted<BakedModel>> models) implements BakedModel {
+    @Override
+    public List<QuadView> getQuads(@Nullable IBlockAccess world, int x, int y, int z, Block block, int meta, ForgeDirection dir, Random random, int color, @Nullable Supplier<QuadView> quadPool) {
+        return Weighted.selectOne(models, random).getQuads(world, x, y, z, block, meta, dir, random, color, quadPool);
+    }
 }
