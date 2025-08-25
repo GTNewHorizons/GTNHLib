c84e8d307a11ee3a11e950d3e76c7c9aeb06f97a
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2025-08-25 18:33:15 -0400

    Use unmodifiable ArrayMaps for multipart models
    They're faster for this purpose and guaranteed to be thread-safe when iterated concurrently

--- src/main/java/com/gtnewhorizon/gtnhlib/client/model/baked/MultipartModel.java
+++ src/main/java/com/gtnewhorizon/gtnhlib/client/model/baked/MultipartModel.java
@@ -1,29 +1,41 @@
 package com.gtnewhorizon.gtnhlib.client.model.baked;
 
-import com.gtnewhorizon.gtnhlib.client.model.BakedModel;
-import com.gtnewhorizon.gtnhlib.client.model.state.MultipartState.Case.Condition;
-import com.gtnewhorizon.gtnhlib.client.renderer.quad.QuadView;
-import it.unimi.dsi.fastutil.objects.Object2ObjectMap;
-import it.unimi.dsi.fastutil.objects.Object2ObjectMaps;
-import it.unimi.dsi.fastutil.objects.ObjectArrayList;
+import static it.unimi.dsi.fastutil.objects.Object2ObjectMaps.unmodifiable;
+
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
 import java.util.function.Supplier;
+
 import net.minecraft.block.Block;
 import net.minecraft.world.IBlockAccess;
 import net.minecraftforge.common.util.ForgeDirection;
+
 import org.jetbrains.annotations.Nullable;
 
-public record MultipartModel(Object2ObjectMap<Condition, BakedModel> piles)
-    implements BakedModel {
+import com.gtnewhorizon.gtnhlib.client.model.BakedModel;
+import com.gtnewhorizon.gtnhlib.client.model.state.MultipartState.Case.Condition;
+import com.gtnewhorizon.gtnhlib.client.renderer.quad.QuadView;
+
+import it.unimi.dsi.fastutil.objects.Object2ObjectArrayMap;
+import it.unimi.dsi.fastutil.objects.Object2ObjectMaps;
+import it.unimi.dsi.fastutil.objects.Object2ObjectMaps.UnmodifiableMap;
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
+
+public record MultipartModel(UnmodifiableMap<Condition, BakedModel> piles) implements BakedModel {
+
+    public MultipartModel(Object2ObjectArrayMap<Condition, BakedModel> piles) {
+        this((UnmodifiableMap<Condition, BakedModel>) unmodifiable(piles));
+    }
 
     @Override
-    public List<QuadView> getQuads(@Nullable IBlockAccess world, int x, int y, int z, Block block, int meta, ForgeDirection dir, Random random, int color, @Nullable Supplier<QuadView> quadPool) {
+    public List<QuadView> getQuads(@Nullable IBlockAccess world, int x, int y, int z, Block block, int meta,
+            ForgeDirection dir, Random random, int color, @Nullable Supplier<QuadView> quadPool) {
         final var quads = new ObjectArrayList<QuadView>();
         Object2ObjectMaps.fastForEach(piles, e -> {
-            if (e.getKey().matches(stateMap(meta))) quads.addAll(e.getValue().getQuads(world, x, y, z, block, meta, dir, random, color, quadPool));
+            if (e.getKey().matches(stateMap(meta)))
+                quads.addAll(e.getValue().getQuads(world, x, y, z, block, meta, dir, random, color, quadPool));
         });
         return quads;
     }
