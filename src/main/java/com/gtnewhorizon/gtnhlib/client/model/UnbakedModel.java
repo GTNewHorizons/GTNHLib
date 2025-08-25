4ff6dea4715091901faa7b4861f54353220a3593
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2025-08-25 11:29:53 -0400

    Add default bake data

--- src/main/java/com/gtnewhorizon/gtnhlib/client/model/UnbakedModel.java
+++ src/main/java/com/gtnewhorizon/gtnhlib/client/model/UnbakedModel.java
@@ -2,5 +2,9 @@ package com.gtnewhorizon.gtnhlib.client.model;
 
 public interface UnbakedModel {
 
+    default BakedModel bake() {
+        return bake(BakeData.IDENTITY);
+    }
+
     BakedModel bake(BakeData data);
 }
