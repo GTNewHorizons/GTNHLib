ca521ac8c28ddc44394d975f5930328d711ef80f
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2025-08-29 09:52:09 -0400

    Fix NPE

--- src/main/java/com/gtnewhorizon/gtnhlib/client/model/json/MissingModel.java
+++ src/main/java/com/gtnewhorizon/gtnhlib/client/model/json/MissingModel.java
@@ -4,13 +4,14 @@ import static com.gtnewhorizon.gtnhlib.client.model.baked.PileOfQuads.BLANK;
 
 import com.gtnewhorizon.gtnhlib.client.model.BakeData;
 import com.gtnewhorizon.gtnhlib.client.model.BakedModel;
+import java.util.HashMap;
 
 public class MissingModel extends JSONModel {
 
     public static final MissingModel MISSING_MODEL = new MissingModel();
 
     public MissingModel() {
-        super(null, false, null, null, null);
+        super(null, false, null, new HashMap<>(), null);
     }
 
     @Override
