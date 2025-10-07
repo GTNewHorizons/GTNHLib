e06c29cda49dc70372f459da057375d939bb6ed2
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2025-10-07 00:06:47 -0400

    Yet More Unused Code

--- src/main/java/com/gtnewhorizon/gtnhlib/client/renderer/quad/Axis.java
+++ src/main/java/com/gtnewhorizon/gtnhlib/client/renderer/quad/Axis.java
@@ -1,32 +1,10 @@
 package com.gtnewhorizon.gtnhlib.client.renderer.quad;
 
-import net.minecraftforge.common.util.ForgeDirection;
-
-import com.gtnewhorizon.gtnhlib.client.renderer.quad.properties.ModelQuadFacing;
-
 public enum Axis {
 
     X,
     Y,
     Z;
-
-    public static Axis fromDirection(ModelQuadFacing dir) {
-        return switch (dir) {
-            case DOWN, UP -> Y;
-            case NORTH, SOUTH -> Z;
-            case WEST, EAST -> X;
-            case UNASSIGNED -> null;
-        };
-    }
-
-    public static Axis fromDirection(ForgeDirection dir) {
-        return switch (dir) {
-            case DOWN, UP -> Y;
-            case NORTH, SOUTH -> Z;
-            case WEST, EAST -> X;
-            case UNKNOWN -> null;
-        };
-    }
 
     public static Axis fromName(String dir) {
         return switch (dir) {
