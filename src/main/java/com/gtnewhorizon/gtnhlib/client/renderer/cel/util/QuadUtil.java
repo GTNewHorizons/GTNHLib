b4496fd07beeedeed1e6fabe91f5fefda98107f3
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2025-10-07 20:40:58 -0400

    yet more dead removal

--- src/main/java/com/gtnewhorizon/gtnhlib/client/renderer/cel/util/QuadUtil.java
+++ src/main/java/com/gtnewhorizon/gtnhlib/client/renderer/cel/util/QuadUtil.java
@@ -9,11 +9,8 @@ import static com.gtnewhorizon.gtnhlib.client.renderer.cel.model.quad.properties
 import static java.lang.Math.abs;
 import static java.lang.Math.max;
 
-import org.joml.Vector3f;
-
 import com.gtnewhorizon.gtnhlib.client.renderer.cel.api.util.NormI8;
 import com.gtnewhorizon.gtnhlib.client.renderer.cel.model.quad.properties.ModelQuadFacing;
-import com.gtnewhorizon.gtnhlib.client.renderer.cel.render.chunk.vertex.format.ChunkVertexEncoder;
 
 public class QuadUtil {
 
@@ -53,85 +50,4 @@ public class QuadUtil {
         return findNormalFace(NormI8.unpackX(normal), NormI8.unpackY(normal), NormI8.unpackZ(normal));
     }
 
-    public static void calculateNormal(ChunkVertexEncoder.Vertex[] quad, Vector3f result) {
-        ChunkVertexEncoder.Vertex q0 = quad[0], q1 = quad[1], q2 = quad[2], q3 = quad[3];
-
-        final float x0 = q0.x;
-        final float y0 = q0.y;
-        final float z0 = q0.z;
-
-        final float x1 = q1.x;
-        final float y1 = q1.y;
-        final float z1 = q1.z;
-
-        final float x2 = q2.x;
-        final float y2 = q2.y;
-        final float z2 = q2.z;
-
-        final float x3 = q3.x;
-        final float y3 = q3.y;
-        final float z3 = q3.z;
-
-        final float dx0 = x2 - x0;
-        final float dy0 = y2 - y0;
-        final float dz0 = z2 - z0;
-        final float dx1 = x3 - x1;
-        final float dy1 = y3 - y1;
-        final float dz1 = z3 - z1;
-
-        float normX = dy0 * dz1 - dz0 * dy1;
-        float normY = dz0 * dx1 - dx0 * dz1;
-        float normZ = dx0 * dy1 - dy0 * dx1;
-
-        float l = (float) Math.sqrt(normX * normX + normY * normY + normZ * normZ);
-
-        if (l != 0) {
-            normX /= l;
-            normY /= l;
-            normZ /= l;
-        }
-
-        result.set(normX, normY, normZ);
-    }
-
-    public static int calculateNormal(ChunkVertexEncoder.Vertex[] quad) {
-        ChunkVertexEncoder.Vertex q0 = quad[0], q1 = quad[1], q2 = quad[2], q3 = quad[3];
-
-        final float x0 = q0.x;
-        final float y0 = q0.y;
-        final float z0 = q0.z;
-
-        final float x1 = q1.x;
-        final float y1 = q1.y;
-        final float z1 = q1.z;
-
-        final float x2 = q2.x;
-        final float y2 = q2.y;
-        final float z2 = q2.z;
-
-        final float x3 = q3.x;
-        final float y3 = q3.y;
-        final float z3 = q3.z;
-
-        final float dx0 = x2 - x0;
-        final float dy0 = y2 - y0;
-        final float dz0 = z2 - z0;
-        final float dx1 = x3 - x1;
-        final float dy1 = y3 - y1;
-        final float dz1 = z3 - z1;
-
-        float normX = dy0 * dz1 - dz0 * dy1;
-        float normY = dz0 * dx1 - dx0 * dz1;
-        float normZ = dx0 * dy1 - dy0 * dx1;
-
-        float l = (float) Math.sqrt(normX * normX + normY * normY + normZ * normZ);
-
-        if (l != 0) {
-            normX /= l;
-            normY /= l;
-            normZ /= l;
-        }
-
-        return NormI8.pack(normX, normY, normZ);
-    }
 }
