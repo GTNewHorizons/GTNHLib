29945449801d8c89453dcd96d1ca6ce5a00782ca
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2025-10-20 10:37:48 -0400

    move ifaces

--- src/main/java/com/gtnewhorizon/gtnhlib/client/model/baked/PileOfQuads.java
+++ src/main/java/com/gtnewhorizon/gtnhlib/client/model/baked/PileOfQuads.java
@@ -21,7 +21,6 @@ import net.minecraft.world.IBlockAccess;
 import org.jetbrains.annotations.Nullable;
 
 import com.github.bsideup.jabel.Desugar;
-import com.gtnewhorizon.gtnhlib.client.model.BakedModel;
 import com.gtnewhorizon.gtnhlib.client.renderer.cel.model.quad.ModelQuadView;
 import com.gtnewhorizon.gtnhlib.client.renderer.cel.model.quad.ModelQuadViewMutable;
 import com.gtnewhorizon.gtnhlib.client.renderer.cel.model.quad.properties.ModelQuadFacing;
