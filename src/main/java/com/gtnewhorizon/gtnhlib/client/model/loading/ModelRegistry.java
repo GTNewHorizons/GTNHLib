da6488ffc674d349550e491dfb52387fa02861a7
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2025-10-19 21:49:19 -0400

    Move blockstate to more sensible spot

--- src/main/java/com/gtnewhorizon/gtnhlib/client/model/loading/ModelRegistry.java
+++ src/main/java/com/gtnewhorizon/gtnhlib/client/model/loading/ModelRegistry.java
@@ -7,7 +7,7 @@ import net.minecraft.block.Block;
 import com.github.bsideup.jabel.Desugar;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.gtnewhorizon.gtnhlib.block.BlockState;
+import com.gtnewhorizon.gtnhlib.client.model.state.BlockState;
 import com.gtnewhorizon.gtnhlib.concurrent.ThreadsafeCache;
 import com.gtnewhorizon.gtnhlib.client.model.BakedModel;
 import com.gtnewhorizon.gtnhlib.client.model.json.JSONModel;
