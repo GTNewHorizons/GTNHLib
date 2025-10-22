9e5b437171570e5cd136ab419ea012482f35525b
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2025-10-21 23:10:59 -0400

    Register config in coremod

--- src/main/java/com/gtnewhorizon/gtnhlib/CommonProxy.java
+++ src/main/java/com/gtnewhorizon/gtnhlib/CommonProxy.java
@@ -1,13 +1,6 @@
 package com.gtnewhorizon.gtnhlib;
 
-import net.minecraft.entity.player.EntityPlayerMP;
-import net.minecraft.launchwrapper.Launch;
-import net.minecraft.server.MinecraftServer;
-import net.minecraft.util.IChatComponent;
-import net.minecraftforge.common.util.FakePlayer;
-
 import com.gtnewhorizon.gtnhlib.block.BlockTest;
-import com.gtnewhorizon.gtnhlib.config.ConfigException;
 import com.gtnewhorizon.gtnhlib.config.ConfigurationManager;
 import com.gtnewhorizon.gtnhlib.eventbus.AutoEventBus;
 import com.gtnewhorizon.gtnhlib.eventbus.EventBusSubscriber;
@@ -16,7 +9,6 @@ import com.gtnewhorizon.gtnhlib.keybind.SyncedKeybind;
 import com.gtnewhorizon.gtnhlib.network.NetworkHandler;
 import com.gtnewhorizon.gtnhlib.network.PacketMessageAboveHotbar;
 import com.gtnewhorizon.gtnhlib.network.PacketViewDistance;
-
 import cpw.mods.fml.common.event.FMLConstructionEvent;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
@@ -29,6 +21,11 @@ import cpw.mods.fml.common.event.FMLServerStoppingEvent;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.gameevent.PlayerEvent;
 import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.launchwrapper.Launch;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.IChatComponent;
+import net.minecraftforge.common.util.FakePlayer;
 
 @EventBusSubscriber
 public class CommonProxy {
@@ -40,11 +37,6 @@ public class CommonProxy {
     public void preInit(FMLPreInitializationEvent event) {
         AutoEventBus.executePhase(Phase.PRE);
         GTNHLib.info("GTNHLib version " + Tags.VERSION + " loaded.");
-        try {
-            ConfigurationManager.registerConfig(GTNHLibConfig.class);
-        } catch (ConfigException e) {
-            GTNHLib.LOG.error("Failed to register GTNHLib config!", e);
-        }
 
         if (GTNHLibConfig.enableTestBlocks) {
             GameRegistry.registerBlock(new BlockTest(), "model_test");
