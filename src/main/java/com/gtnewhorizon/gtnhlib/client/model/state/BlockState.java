45b10c26ea2bb1a9789ddf4e08b581c28fb986c8
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2025-10-23 10:34:29 -0400

    Unwind records for better Java 8 compat

--- src/main/java/com/gtnewhorizon/gtnhlib/client/model/state/BlockState.java
+++ src/main/java/com/gtnewhorizon/gtnhlib/client/model/state/BlockState.java
@@ -1,8 +1,37 @@
 package com.gtnewhorizon.gtnhlib.client.model.state;
 
+import java.util.Objects;
+
 import net.minecraft.block.Block;
 
-import com.github.bsideup.jabel.Desugar;
+public final class BlockState {
 
-@Desugar
-public record BlockState(Block block, int meta) {}
+    private final Block block;
+    private final int meta;
+
+    public BlockState(Block block, int meta) {
+        this.block = block;
+        this.meta = meta;
+    }
+
+    public Block block() {
+        return block;
+    }
+
+    public int meta() {
+        return meta;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        var that = (BlockState) obj;
+        return Objects.equals(this.block, that.block) && this.meta == that.meta;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(block, meta);
+    }
+}
