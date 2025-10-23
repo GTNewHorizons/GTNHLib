6ecf57b7192054cb7b4bf7aab0350155d896a9c1
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2025-10-22 20:03:46 -0400

    Make test block rotateable

--- src/main/java/com/gtnewhorizon/gtnhlib/block/BlockTest.java
+++ src/main/java/com/gtnewhorizon/gtnhlib/block/BlockTest.java
@@ -4,11 +4,26 @@ import static com.gtnewhorizon.gtnhlib.client.model.ModelISBRH.JSON_ISBRH_ID;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.world.World;
+import net.minecraftforge.common.util.ForgeDirection;
+import org.jetbrains.annotations.NotNull;
 
 public class BlockTest extends Block {
 
     public BlockTest() {
         super(Material.wood);
+    }
+
+    @Override
+    public int onBlockPlaced(@NotNull World worldIn, int x, int y, int z, int side, float subX, float subY, float subZ, int meta) {
+
+        // Face NORTH if placed up or down
+        final var s = ForgeDirection.getOrientation(side);
+        if (s == ForgeDirection.UP || s == ForgeDirection.DOWN)
+            return 2;
+
+        // Face the placed side
+        return side - 2;
     }
 
     @Override
