a4be7bab723ece3080e999a70893bf188f49950b
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2025-10-22 20:04:05 -0400

    Make JSONVariant use degrees

--- src/main/java/com/gtnewhorizon/gtnhlib/client/model/JSONVariant.java
+++ src/main/java/com/gtnewhorizon/gtnhlib/client/model/JSONVariant.java
@@ -1,33 +1,20 @@
 package com.gtnewhorizon.gtnhlib.client.model;
 
-import static java.lang.Math.toRadians;
-
+import com.github.bsideup.jabel.Desugar;
+import com.gtnewhorizon.gtnhlib.client.model.loading.ResourceLoc;
 import org.jetbrains.annotations.ApiStatus.Internal;
 import org.joml.Matrix4f;
 
-import com.github.bsideup.jabel.Desugar;
-import com.gtnewhorizon.gtnhlib.client.model.loading.ResourceLoc;
-
-/**
- * Largely mirrors Minecraft's format for specifying models, except we use radians instead of degrees.
- *
- * @param model  The model file this variant refers to
- * @param x      X rotation, in radians
- * @param y      Y rotation, in radians
- * @param z      Z rotation, in radians
- * @param uvLock If true, textures aren't rotated with the block
- */
+/// Largely mirrors Minecraft's format for specifying models.
+///
+/// @param model  The model file this variant refers to
+/// @param x      X rotation
+/// @param y      Y rotation
+/// @param z      Z rotation (not present in modern)
+/// @param uvLock If true, textures aren't rotated with the block
 @Desugar
 @Internal
 public record JSONVariant(ResourceLoc.ModelLoc model, float x, float y, float z, boolean uvLock) implements BakeData {
-
-    public JSONVariant(ResourceLoc.ModelLoc model, int x, int y, boolean uvLock) {
-        this(model, x, y, 0, uvLock);
-    }
-
-    public JSONVariant(ResourceLoc.ModelLoc model, int x, int y, int z, boolean uvLock) {
-        this(model, (float) toRadians(x), (float) toRadians(y), (float) toRadians(z), uvLock);
-    }
 
     /// @param weight Used when selecting models. Irrelevant when only one variant is allowed, but must always be
     /// greater than zero.
@@ -37,11 +24,16 @@ public record JSONVariant(ResourceLoc.ModelLoc model, float x, float y, float z,
         return new Weighted<>(v, weight);
     }
 
+    private static final float DEG2RAD = (float) (Math.PI / 180);
+
     @Override
     public Matrix4f getAffineMatrix() {
-
-        return new Matrix4f().translation(-.5f, -.5f, -.5f).rotateLocalX(x).rotateLocalY(y).rotateLocalZ(z)
-                .translateLocal(.5f, .5f, .5f);
+        return new Matrix4f()
+            .translation(-.5f, -.5f, -.5f)
+            .rotateLocalX(x * DEG2RAD)
+            .rotateLocalY(y * DEG2RAD)
+            .rotateLocalZ(z * DEG2RAD)
+            .translateLocal(.5f, .5f, .5f);
     }
 
     @Override
