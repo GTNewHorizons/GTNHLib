b2b5e631c3d89e1c82dadf129335c74aa7d0d0fe
RecursivePineapple <recursive_pineapple@proton.me>
2025-11-02 16:06:37 -0500

    Add Generic Item I/O Capabilities (#167)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Martin Robertz <dream-master@gmx.net>

--- src/main/java/com/gtnewhorizon/gtnhlib/util/data/ImmutableItemMeta.java
+++ src/main/java/com/gtnewhorizon/gtnhlib/util/data/ImmutableItemMeta.java
@@ -3,10 +3,11 @@ package com.gtnewhorizon.gtnhlib.util.data;
 import javax.annotation.Nonnull;
 
 import net.minecraft.block.Block;
-import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.oredict.OreDictionary;
+
+import com.gtnewhorizon.gtnhlib.util.ItemUtil;
 
 /**
  * An immutable item-meta pair. This must not be cast down to its mutable version unless you have a very good reason. It
@@ -17,51 +18,51 @@ public interface ImmutableItemMeta {
 
     /**
      * The value of this must not change while this object is exposed via an API.
-     * 
+     *
      * @return The item stored in this pair.
      */
     @Nonnull
-    public Item getItem();
+    Item getItem();
 
     /**
      * The value of this must not change while this object is exposed via an API.
-     * 
+     *
      * @return The item's metadata stored in this pair. May be {@link OreDictionary#WILDCARD_VALUE}.
      */
-    public int getItemMeta();
+    int getItemMeta();
 
     /**
      * Gets the corresponding block for this item. Subclasses may provide a faster implementation.
      */
-    public default Block getBlock() {
+    default Block getBlock() {
         return Block.getBlockFromItem(getItem());
     }
 
     /**
      * Checks if this pair matches the given ItemStack's item and metadata.
      */
-    public default boolean matches(ItemStack stack) {
+    default boolean matches(ItemStack stack) {
         if (stack == null) return false;
 
-        return matches(stack.getItem(), Items.feather.getDamage(stack));
+        return matches(stack.getItem(), ItemUtil.getStackMeta(stack));
     }
 
     /**
      * Checks if this pair matches the given item & meta.
-     * 
-     * @param Item The item.
+     *
+     * @param item The item.
      * @param meta The meta. If this parameter or {@link #getItemMeta()} equals {@link OreDictionary#WILDCARD_VALUE}
      *             then meta checks are ignored.
      * @return Whether this pair matches or not.
      */
-    public default boolean matches(Item item, int meta) {
+    default boolean matches(Item item, int meta) {
         return getItem() == item
                 && (meta == OreDictionary.WILDCARD_VALUE || getItemMeta() == OreDictionary.WILDCARD_VALUE
                         || getItemMeta() == meta);
     }
 
     /** Converts this pair to an ItemStack. */
-    public default ItemStack toStack(int amount) {
+    default ItemStack toStack(int amount) {
         int meta = getItemMeta();
 
         return new ItemStack(getItem(), amount, meta == OreDictionary.WILDCARD_VALUE ? 0 : meta);
