91b2fcbe5da0c2cae27607509655e008e38c122b
RecursivePineapple <recursive_pineapple@proton.me>
2025-11-06 19:19:25 -0500

    Add simple source/sink/io

---
+++ src/main/java/com/gtnewhorizon/gtnhlib/item/SimpleItemSource.java
@@ -0,0 +1,45 @@
+package com.gtnewhorizon.gtnhlib.item;
+
+import net.minecraft.item.ItemStack;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import com.gtnewhorizon.gtnhlib.capability.item.ItemSource;
+
+public abstract class SimpleItemSource implements ItemSource {
+
+    protected int[] allowedSourceSlots;
+
+    @Override
+    public void setAllowedSourceSlots(int @Nullable [] slots) {
+        allowedSourceSlots = slots;
+    }
+
+    @Override
+    public @Nullable ItemStack pull(@Nullable ItemStackPredicate filter, @Nullable ItemStack2IntFunction amount) {
+        InventoryIterator iter = sourceIterator();
+
+        while (iter.hasNext()) {
+            ImmutableItemStack stack = iter.next();
+
+            if (stack == null || stack.isEmpty()) continue;
+
+            if (filter == null || filter.test(stack)) {
+                int toExtract = amount == null ? stack.getStackSize() : amount.apply(stack);
+
+                return iter.extract(toExtract, false);
+            }
+        }
+
+        return null;
+    }
+
+    @Override
+    public @NotNull InventoryIterator sourceIterator() {
+        return iterator(allowedSourceSlots);
+    }
+
+    @NotNull
+    protected abstract InventoryIterator iterator(int[] allowedSlots);
+}
