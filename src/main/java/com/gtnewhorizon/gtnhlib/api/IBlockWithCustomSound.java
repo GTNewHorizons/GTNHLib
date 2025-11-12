50379742a324ceeceb05b50094808ff345440a8d
RecursivePineapple <recursive_pineapple@proton.me>
2025-11-12 04:29:44 -0500

    Add ability to dynamically control block sounds (#195)

---
+++ src/main/java/com/gtnewhorizon/gtnhlib/api/IBlockWithCustomSound.java
@@ -0,0 +1,11 @@
+package com.gtnewhorizon.gtnhlib.api;
+
+import net.minecraft.block.Block;
+import net.minecraft.world.World;
+
+/// Implement this interface on a [Block] to make its sound dependent on some world state (meta, tile entities,
+/// etc). This may be run on the server, so SideOnly(CLIENT) code cannot be used in this method.
+public interface IBlockWithCustomSound {
+
+    Block.SoundType getSound(World world, int x, int y, int z);
+}
