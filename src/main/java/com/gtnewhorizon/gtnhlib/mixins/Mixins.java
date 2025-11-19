ab9fd3c7448c48d8c7837d813052a6dd3cca84c0
GitHub GTNH Actions <>
2025-11-19 04:11:24 +0000

    spotlessApply

--- src/main/java/com/gtnewhorizon/gtnhlib/mixins/Mixins.java
+++ src/main/java/com/gtnewhorizon/gtnhlib/mixins/Mixins.java
@@ -38,7 +38,7 @@ public enum Mixins implements IMixins {
                     "block_sounds.MixinPlayerControllerMP")
             .addClientMixins("block_sounds.MixinRenderGlobal").setPhase(Phase.EARLY)
             .setApplyIf(() -> GTNHLibConfig.blockSoundMixins)),
-      ENTITY_RENDERER_ACCESSOR(new MixinBuilder("Accesses the lightmap property of EntityRenderer").setPhase(Phase.EARLY)
+    ENTITY_RENDERER_ACCESSOR(new MixinBuilder("Accesses the lightmap property of EntityRenderer").setPhase(Phase.EARLY)
             .addCommonMixins("EntityRendererAccessor")),
     //
     ;
