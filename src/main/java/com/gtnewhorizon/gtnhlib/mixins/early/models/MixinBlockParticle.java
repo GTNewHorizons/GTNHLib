7ba1a7de19c5ca27ad054fb40237aecbabffbda4
Ruisu <akitomoya2908@gmail.com>
2025-11-30 15:17:46 +0700

    add new model particle use getIcon

--- src/main/java/com/gtnewhorizon/gtnhlib/mixins/early/models/MixinBlockParticle.java
+++ src/main/java/com/gtnewhorizon/gtnhlib/mixins/early/models/MixinBlockParticle.java
@@ -1,34 +1,34 @@
 package com.gtnewhorizon.gtnhlib.mixins.early.models;
 
 import net.minecraft.block.Block;
-import net.minecraft.client.particle.EffectRenderer;
-import net.minecraft.util.MovingObjectPosition;
-import net.minecraft.world.World;
+import net.minecraft.util.IIcon;
+import net.minecraft.world.IBlockAccess;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import com.gtnewhorizon.gtnhlib.client.model.ModelISBRH;
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 
 @Mixin(Block.class)
 public class MixinBlockParticle {
 
-    @Inject(method = "addDestroyEffects", at = @At("HEAD"), cancellable = true, remap = false)
-    public void gtnhlib$addDestroyEffects(World world, int x, int y, int z, int meta, EffectRenderer effectRenderer,
-            CallbackInfoReturnable<Boolean> cir) {
-        if (((Block) (Object) this).getRenderType() == ModelISBRH.JSON_ISBRH_ID) {
-            cir.setReturnValue(ModelISBRH.INSTANCE.addDestroyEffects(world, x, y, z, meta, effectRenderer));
+    @WrapMethod(method = "getIcon(Lnet/minecraft/world/IBlockAccess;IIII)Lnet/minecraft/util/IIcon;")
+    private IIcon nhlib$wrapGetIcon(IBlockAccess world, int x, int y, int z, int side, Operation<IIcon> original) {
+        Block block = (Block) (Object) this;
+        int meta = world.getBlockMetadata(x, y, z);
+        if (block.getRenderType() == ModelISBRH.JSON_ISBRH_ID) {
+            return ModelISBRH.INSTANCE.getParticleIcon(block, world, x, y, z, meta);
         }
+        return original.call(world, x, y, z, side);
     }
 
-    @Inject(method = "addHitEffects", at = @At("HEAD"), cancellable = true, remap = false)
-    public void gtnhlib$addHitEffects(World worldObj, MovingObjectPosition target, EffectRenderer effectRenderer,
-            CallbackInfoReturnable<Boolean> cir) {
-        if (((Block) (Object) this).getRenderType() == ModelISBRH.JSON_ISBRH_ID) {
-            cir.setReturnValue(ModelISBRH.INSTANCE.addHitEffects(worldObj, target, effectRenderer));
+    @WrapMethod(method = "getIcon(II)Lnet/minecraft/util/IIcon;")
+    private IIcon nhlib$wrapGetIcon(int side, int meta, Operation<IIcon> original) {
+        Block block = (Block) (Object) this;
+        if (block.getRenderType() == ModelISBRH.JSON_ISBRH_ID) {
+            return ModelISBRH.INSTANCE.getParticleIcon(block, null, 0, 0, 0, meta);
         }
+        return original.call(meta, side);
     }
-
 }
