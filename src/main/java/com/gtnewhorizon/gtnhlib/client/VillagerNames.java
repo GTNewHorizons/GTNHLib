cdb9a7187a58fa7cae7a3da3c210d2e658a3da57
SuperSoupr <26164930+SuperSoupr@users.noreply.github.com>
2025-12-09 02:20:23 +0200

    Move villager names from WAWLA

---
+++ src/main/java/com/gtnewhorizon/gtnhlib/client/VillagerNames.java
@@ -0,0 +1,37 @@
+package com.gtnewhorizon.gtnhlib.client;
+
+import cpw.mods.fml.common.registry.VillagerRegistry;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.util.ResourceLocation;
+
+// Moved from wawla's net.darkhax.wawla.util.Utilities
+public class VillagerNames {
+    /**
+     * Lists of names for the vanilla villagers.
+     */
+    @SideOnly(Side.CLIENT)
+    private static String[] vanillaVillagers = { "farmer", "librarian", "priest", "blacksmith", "butcher" };
+
+    /**
+     * Retrieves a unique string related to the texture name of a villager. This allows for villagers to be
+     * differentiated based on their profession rather than their ID.
+     *
+     * @param id : The ID of the villager being looked up.
+     * @return String: The texture name, minus file path and extension.
+     */
+    @SideOnly(Side.CLIENT)
+    public static String getVillagerName(int id) {
+
+        ResourceLocation skin = VillagerRegistry.getVillagerSkin(id, null);
+        return (id >= 0
+            && id <= 4)
+            ? vanillaVillagers[id]
+            : (skin != null)
+            ? skin.getResourceDomain() + "."
+            + skin.getResourcePath().substring(
+            skin.getResourcePath().lastIndexOf("/") + 1,
+            skin.getResourcePath().length() - 4)
+            : "misingno";
+    }
+}
