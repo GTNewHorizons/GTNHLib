7a02150d57cd22fb57ff6a007070f042dd5ce98b
hiroscho <hiroscho.games@gmail.com>
2025-12-23 12:22:22 +0100

    simulate DSU

--- src/main/java/com/gtnewhorizon/gtnhlib/item/impl/mfr/DSUItemIO.java
+++ src/main/java/com/gtnewhorizon/gtnhlib/item/impl/mfr/DSUItemIO.java
@@ -5,6 +5,7 @@ import org.jetbrains.annotations.NotNull;
 import com.gtnewhorizon.gtnhlib.item.InventoryIterator;
 import com.gtnewhorizon.gtnhlib.item.SimpleItemIO;
 
+import org.jetbrains.annotations.Nullable;
 import powercrystals.minefactoryreloaded.api.IDeepStorageUnit;
 
 public class DSUItemIO extends SimpleItemIO {
@@ -17,6 +18,11 @@ public class DSUItemIO extends SimpleItemIO {
 
     @Override
     protected @NotNull InventoryIterator iterator(int[] allowedSlots) {
-        return new DSUInventoryIterator(dsu, allowedSlots);
+        return new DSUInventoryIterator(dsu, allowedSlots, false);
+    }
+
+    @Override
+    public @Nullable InventoryIterator simulatedSinkIterator() {
+        return new DSUInventoryIterator(dsu, allowedSinkSlots, true);
     }
 }
