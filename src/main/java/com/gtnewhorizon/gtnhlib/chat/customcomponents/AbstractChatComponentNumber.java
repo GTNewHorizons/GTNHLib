87dfdbefca82c3297833595a1e6d086126fcdef3
GTNH-Colen <54497873+GTNH-Colen@users.noreply.github.com>
2025-12-31 03:14:43 +0000

    Custom chat component system + number chat component.

---
+++ src/main/java/com/gtnewhorizon/gtnhlib/chat/customcomponents/AbstractChatComponentNumber.java
@@ -0,0 +1,44 @@
+package com.gtnewhorizon.gtnhlib.chat.customcomponents;
+
+import java.math.BigDecimal;
+
+import org.jetbrains.annotations.NotNull;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.gtnewhorizon.gtnhlib.chat.AbstractChatComponentCustom;
+import com.gtnewhorizon.gtnhlib.chat.ChatUtility;
+import com.gtnewhorizon.gtnhlib.util.numberformatting.NumberFormatUtil;
+
+public abstract class AbstractChatComponentNumber extends AbstractChatComponentCustom {
+
+    protected BigDecimal number = BigDecimal.ZERO;
+
+    public AbstractChatComponentNumber() {}
+
+    public AbstractChatComponentNumber(Number number) {
+        this.number = NumberFormatUtil.bigDecimalConverter(number);
+    }
+
+    protected abstract String formatNumber(Number value);
+
+    @Override
+    public final String getUnformattedTextForChat() {
+        return formatNumber(number);
+    }
+
+    @Override
+    public @NotNull JsonElement serialize() {
+        final JsonObject obj = new JsonObject();
+        ChatUtility.serializeNumber(obj, number);
+
+        return obj;
+    }
+
+    @Override
+    public void deserialize(@NotNull JsonElement jsonElement) {
+        JsonObject obj = jsonElement.getAsJsonObject();
+        this.number = ChatUtility.deserializeNumber(obj);
+    }
+
+}
