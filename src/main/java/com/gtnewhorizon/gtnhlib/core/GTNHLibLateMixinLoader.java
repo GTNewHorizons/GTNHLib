da534c32f2b33a0f093b95a996323f9f5b2b548e
LazyFleshWasTaken <40587985+LazyFleshWasTaken@users.noreply.github.com>
2026-02-12 12:30:15 -0800

    Add an enhanced infusion recipe that supports replacements (#270)
    Co-authored-by: koolkrafter5 <creepersrgreen@gmail.com>

---
+++ src/main/java/com/gtnewhorizon/gtnhlib/core/GTNHLibLateMixinLoader.java
@@ -0,0 +1,23 @@
+package com.gtnewhorizon.gtnhlib.core;
+
+import java.util.List;
+import java.util.Set;
+
+import com.gtnewhorizon.gtnhlib.mixins.Mixins;
+import com.gtnewhorizon.gtnhmixins.ILateMixinLoader;
+import com.gtnewhorizon.gtnhmixins.LateMixin;
+import com.gtnewhorizon.gtnhmixins.builders.IMixins;
+
+@LateMixin
+public class GTNHLibLateMixinLoader implements ILateMixinLoader {
+
+    @Override
+    public String getMixinConfig() {
+        return "mixins.gtnhlib.late.json";
+    }
+
+    @Override
+    public List<String> getMixins(Set<String> loadedMods) {
+        return IMixins.getLateMixins(Mixins.class, loadedMods);
+    }
+}
