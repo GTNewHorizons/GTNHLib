a2682994b5994ea7bc1b5ab6c0e436f0d44dab7f
LazyFleshWasTaken <40587985+LazyFleshWasTaken@users.noreply.github.com>
2026-02-13 19:29:10 -0800

    pr farming time... adds a method to automaticall make and register an EIR
    really should think of this earlier

--- src/main/java/com/gtnewhorizon/gtnhlib/api/thaumcraft/EnhancedInfusionRecipe.java
+++ src/main/java/com/gtnewhorizon/gtnhlib/api/thaumcraft/EnhancedInfusionRecipe.java
@@ -6,6 +6,7 @@ import net.minecraft.item.ItemStack;
 
 import com.github.bsideup.jabel.Desugar;
 
+import thaumcraft.api.ThaumcraftApi;
 import thaumcraft.api.aspects.AspectList;
 import thaumcraft.api.crafting.InfusionRecipe;
 
@@ -35,6 +36,29 @@ public class EnhancedInfusionRecipe extends InfusionRecipe {
         this.replacements = replacements;
     }
 
+    /**
+     * Automatically create and register an Enhanced Infusion Recipe, which is then returned for use, for example in a research page
+     * @param research     The required research for this infusion
+     * @param output       The item created by this infusion
+     * @param inst         The instability of this infusion
+     * @param aspects2     The required essentia for this infusion
+     * @param input        The item required to be placed on the central pedestal
+     * @param recipe       The items required to be placed on the outer pedestals
+     * @param replacements What items to replace consumed items with on the outer pedestals
+     * @return             The Enhanced infusion recipe created
+     */
+
+    public static EnhancedInfusionRecipe addEnhancedInfusionCraftingRecipe (String research, Object output, int inst, AspectList aspects2, ItemStack input, ItemStack[] recipe, List<Replacement> replacements) {
+        if(!(output instanceof ItemStack) && !(output instanceof Object[])) {
+            return null;
+        } else {
+            EnhancedInfusionRecipe r = new EnhancedInfusionRecipe(research, output, inst, aspects2, input, recipe, replacements);
+            ThaumcraftApi.getCraftingRecipes().add(r);
+            return r;
+        }
+    }
+
+
     public List<Replacement> getReplacements() {
         return this.replacements;
     }
@@ -42,7 +66,8 @@ public class EnhancedInfusionRecipe extends InfusionRecipe {
     /**
      * A Replacement defines one type of ItemStack (e.g. all Iron Ingots) to replace with another ItemStack (e.g.
      * Diamonds). Setting strict to false allows input to match with all metadata if input is given metadata of
-     * Short.MAX_VALUE. NBT data (such as enchantments) is ignored when comparing against input.
+     * Short.MAX_VALUE. NBT data (such as enchantments) is ignored when comparing against input. The output may be null (e.g.
+     * for water buckets and other containers)
      *
      * @param input  The input ItemStack to compare recipe ingredients against
      * @param output The ItemStack to replace items that match the input
