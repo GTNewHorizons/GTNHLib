e7e9cd1b580d2888ed711e543650dc592624633d
Discreater <Discreater@163.com>
2026-02-25 04:58:09 +0800

    Add custom chat components for fluid and item names (#278)
    Co-authored-by: Caedis <Caedis@users.noreply.github.com>

---
+++ src/main/java/com/gtnewhorizon/gtnhlib/chat/customcomponents/ChatComponentFluidName.java
@@ -0,0 +1,68 @@
+package com.gtnewhorizon.gtnhlib.chat.customcomponents;
+
+import java.io.IOException;
+
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.network.PacketBuffer;
+import net.minecraftforge.fluids.FluidStack;
+
+import com.gtnewhorizon.gtnhlib.GTNHLib;
+import com.gtnewhorizon.gtnhlib.chat.AbstractChatComponentCustom;
+
+public class ChatComponentFluidName extends AbstractChatComponentBuffer<ChatComponentFluidName> {
+
+    public FluidStack stack = null;
+
+    public ChatComponentFluidName() {}
+
+    public ChatComponentFluidName(FluidStack stack) {
+        this.stack = stack;
+    }
+
+    @Override
+    public String getID() {
+        return "gtnhlib:ChatComponentFluidName";
+    }
+
+    @Override
+    protected AbstractChatComponentCustom copySelf() {
+        return new ChatComponentFluidName(stack == null ? null : stack.copy());
+    }
+
+    @Override
+    public String getUnformattedTextForChat() {
+        return stack == null ? "" : stack.getLocalizedName();
+    }
+
+    @Override
+    public void encode(PacketBuffer buffer) {
+        if (stack == null) {
+            buffer.writeBoolean(true);
+        } else {
+            buffer.writeBoolean(false);
+            NBTTagCompound fluidStackTag = stack.writeToNBT(new NBTTagCompound());
+            try {
+                buffer.writeNBTTagCompoundToBuffer(fluidStackTag);
+            } catch (IOException e) {
+                GTNHLib.LOG.error("Failed to write fluid stack nbt to buffer: {}", e.getMessage());
+            }
+        }
+    }
+
+    public static FluidStack decodeToStack(PacketBuffer buffer) {
+        if (buffer.readBoolean()) {
+            return null;
+        }
+        try {
+            return FluidStack.loadFluidStackFromNBT(buffer.readNBTTagCompoundFromBuffer());
+        } catch (IOException e) {
+            GTNHLib.LOG.error("Failed to decode fluid stack from buffer: {}", e.getMessage());
+            return null;
+        }
+    }
+
+    @Override
+    public void decode(PacketBuffer buffer) {
+        this.stack = ChatComponentFluidName.decodeToStack(buffer);
+    }
+}
