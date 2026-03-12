243cc02d88a57759bcc2b8a0f7de8e70919c9618
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2026-03-12 00:11:03 -0400

    Fix test block model
    Also refactor registration.

--- src/main/java/com/gtnewhorizon/gtnhlib/test/block/BlockTestTintMul.java
+++ src/main/java/com/gtnewhorizon/gtnhlib/test/block/BlockTestTintMul.java
@@ -5,6 +5,7 @@ import static com.gtnewhorizon.gtnhlib.client.model.ModelISBRH.JSON_ISBRH_ID;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.MathHelper;
@@ -12,15 +13,70 @@ import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import net.minecraftforge.common.util.ForgeDirection;
 
+import com.gtnewhorizon.gtnhlib.blockstate.core.BlockPropertyTrait;
 import com.gtnewhorizon.gtnhlib.blockstate.core.BlockState;
+import com.gtnewhorizon.gtnhlib.blockstate.properties.DirectionBlockProperty;
 import com.gtnewhorizon.gtnhlib.blockstate.registry.BlockPropertyRegistry;
 import com.gtnewhorizon.gtnhlib.client.model.color.IBlockColor;
+
+import cpw.mods.fml.common.registry.GameRegistry;
 
 public class BlockTestTintMul extends Block implements IBlockColor {
 
     public BlockTestTintMul() {
         super(Material.wood);
         setHardness(2f);
+    }
+
+    public static void register() {
+        final var tintMulTestBlock = new BlockTestTintMul();
+        GameRegistry.registerBlock(tintMulTestBlock, "model_test_tint_mul");
+        GameRegistry.registerTileEntity(TileTestTintMul.class, "tile_test_tint_mul");
+        final var property = new DirectionBlockProperty() {
+
+            @Override
+            public String getName() {
+                return "facing";
+            }
+
+            @Override
+            public boolean hasTrait(BlockPropertyTrait trait) {
+                return switch (trait) {
+                    case SupportsWorld, WorldMutable, StackMutable, SupportsStacks -> true;
+                    default -> false;
+                };
+            }
+
+            @Override
+            public ForgeDirection getValue(IBlockAccess world, int x, int y, int z) {
+                TileEntity te = world.getTileEntity(x, y, z);
+                if (te instanceof TileTestTintMul tile) {
+                    return tile.getFacing();
+                }
+                return ForgeDirection.NORTH;
+            }
+
+            @Override
+            public ForgeDirection getValue(Block block, int meta) {
+                return ForgeDirection.NORTH;
+            }
+
+            @Override
+            public void setValue(World world, int x, int y, int z, ForgeDirection value) {
+                TileEntity te = world.getTileEntity(x, y, z);
+                if (te instanceof TileTestTintMul tile) {
+                    tile.setFacing(value);
+                }
+            }
+
+            @Override
+            public ForgeDirection getValue(ItemStack stack) {
+                return ForgeDirection.NORTH;
+            }
+        };
+
+        BlockPropertyRegistry.registerProperty(tintMulTestBlock, property);
+        BlockPropertyRegistry.registerProperty(Item.getItemFromBlock(tintMulTestBlock), property);
     }
 
     @Override
