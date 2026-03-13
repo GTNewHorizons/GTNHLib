fcc887da0887c91edf1f9f00732261e375c98995
__felix__ <134875831+felixfour@users.noreply.github.com>
2026-03-13 05:39:59 -0400

    chaining, docs, more wrapper methods

--- src/main/java/com/gtnewhorizon/gtnhlib/util/BiIterator.java
+++ src/main/java/com/gtnewhorizon/gtnhlib/util/BiIterator.java
@@ -7,77 +7,134 @@ import java.util.function.Function;
 
 public interface BiIterator<E> extends ListIterator<E> {
 
+    /**
+    * Reverses the view of the iterator.
+    */
     void reverse();
 
+    /**
+    * Outputs whether the iterator is reversed.
+    *
+    * @return true if it is reversed, false otherwise
+    */
     boolean isReversed();
 
+    /**
+    * Reverses the view on the iterator.&nbsp;[Read below for impl details][#reversed()]
+    * You should @Override this with the **type of your class** as the return type.
+    * @see java.util.SequencedCollection#reversed()
+    *
+    * @return returns this (the class) for chaining.
+    */
     BiIterator<E> reversed();
 
+    /**
+    * Does nothing unless defined by implementing class.
+    * This is here to not force classes that just want {@link java.util.Iterator#previous()} and {@link java.util.Iterator#next()} to use it.
+    * @see java.util.ListIterator#add(E)
+    *
+    * @param e the object to add to an underlying collection at the cursor position, if implemented
+    */
     @Override
     default void add(E e) {}
 
+    /**
+    * Does nothing unless defined by implementing class.
+    * This is here to not force classes that just want {@link java.util.Iterator#previous()} and {@link java.util.Iterator#next()} to use it.
+    * @see java.util.ListIterator#set(E)
+    *
+    * @param e the object to set in an underlying collection at the cursor position, if implemented
+    */
     @Override
     default void set(E e) {}
 
+    /**
+    * Does nothing unless defined by implementing class.
+    * This is here to not force classes that just want {@link java.util.Iterator#previous()} and {@link java.util.Iterator#next()} to use it.
+    * @see java.util.ListIterator#remove()
+    */
     @Override
     default void remove() {}
 
+    /**
+    * Returns 0 unless defined by implementing class.
+    * This is here to not force classes that just want {@link java.util.Iterator#previous()} and {@link java.util.Iterator#next()} to use it.
+    * It is still a good idea to keep track of iterator index, and {@link IndexedIterator} uses it.
+    * @see java.util.ListIterator#nextIndex()
+    *
+    * @return The index after the cursor if implemented, else 0
+    */
     @Override
     default int nextIndex() {
-        return null;
+        return 0;
     }
 
+    /**
+    * Returns 0 unless defined by implementing class.
+    * This is here to not force classes that just want {@link java.util.Iterator#previous()} and {@link java.util.Iterator#next()} to use it.
+    * It is still a good idea to keep track of iterator index, and {@link IndexedIterator} uses it.
+    * @see java.util.ListIterator#nextIndex()
+    *
+    * @return The index before the cursor if implemented, else 0
+    */
     @Override
     default int previousIndex() {
-        return null;
+        return 0;
     }
 
     /**
-     * Identical to Iterator.forEachRemaining, but supports reversing.
+     * Same functionality as {@link java.util.Iterator#forEachRemaining(java.util.function.Consumer)}, but supports reversing and chaining.
      *
-     * @param action the action that is run on all future elements, in order
+     * @param action the consumer that is run on all future elements, in order
+     * @return the instantiating class, for chaining
      */
-
     @Override
-    default void forEachRemaining(Consumer<? super E> action) {
+    default BiIterator<E> forEachNext(Consumer<? super E> action) {
         if (isReversed()) while (hasPrevious()) action.accept(previous());
         else ListIterator.super.forEachRemaining(action);
+        return this;
     }
 
     /**
-     * Identical to Iterator.forEachRemaining but works backwards.
+     * Same functionality as {@link #forEachNext(java.util.function.Consumer)} but works backwards.
      *
-     * @param action the action that is run on all previous elements, in order
+     * @param action the consumer that is run on all previous elements, in order
+     * @return the instantiating class, for chaining
      */
-    default void forEachPrevious(Consumer<? super E> action) {
+    default BiIterator<E> forEachPrevious(Consumer<? super E> action) {
         if (isReversed()) while (hasNext()) action.accept(next());
         else while (hasPrevious()) action.accept(previous());
+        return this;
     } // Probably COULD have implemented isReversed() with reflection jank. Didn't.
     // in Python, functions are variables so it's a lot easier.
 
     // goddamn java doesn't support private internal default methods
     /**
     * Internal method call for the reversing implementation.&nbsp;Do not use.
-    * @see #forEachRemainingUntil(long, java.util.function.Consumer)
+    * @see #forEachNextUntil(long, java.util.function.Consumer)
     * @hidden
     *
-    * @param flag whether isReversed() should be ignored
+    * @param flag whether {@link #isReversed()} should be ignored
     */
-    default void forEachRemainingUntil(long count, Consumer<? super E> action, boolean flag) {
-        if (isReversed()) count = -count;
-        if (flag && count < 0) forEachPreviousUntil(-count, action, false);
+    default void __forEachNextUntil(long count, Consumer<? super E> action, boolean flag) {
+        if (flag) {
+            if (isReversed()) count = -count;
+            if (count < 0) __forEachPreviousUntil(-count, action, false);
+        }
         else while (hasNext() && count-- > 0) action.accept(next());
     }
 
     /**
-     * Same functionality as forEachRemaining, but stops early after a certian amount of iterations.
-     * Still checks hasNext().
+     * Same functionality as {@link #forEachNext(java.util.function.Consumer)}, but stops early after a certian amount of iterations.
+     * Still checks {@link java.util.Iterator#hasNext()}; use {@link #forEachNextUntil_checkless(long, java.util.function.Consumer)} to not check that.
      *
      * @param count the maximum amount of iterations
-     * @param action the 
+     * @param action the consumer that is run on all previous elements, in order
+     * @return the instantiating class, for chaining
      */
-    default void forEachRemainingUntil(long count, Consumer<? super E> action) {
-        forEachRemainingUntil(count, action, true);
+    default BiIterator<E> forEachNextUntil(long count, Consumer<? super E> action) {
+        __forEachNextUntil(count, action, true);
+        return this;
     }
 
     /**
@@ -85,39 +142,73 @@ public interface BiIterator<E> extends ListIterator<E> {
     * @see #forEachPreviousUntil(long, java.util.function.Consumer)
     * @hidden
     *
-    * @param flag whether isReversed() should be ignored
+    * @param flag whether {@link #isReversed()} should be ignored
     */
-
-    default void forEachPreviousUntil(long count, Consumer<? super E> action, boolean flag) {
-        if (isReversed()) count = -count;
-        if (flag && count < 0) forEachRemainingUntil(-count, action, false);
+    default void __forEachPreviousUntil(long count, Consumer<? super E> action, boolean flag) {
+        if (flag) {
+            if (isReversed()) count = -count;
+            if (count < 0) __forEachNextUntil(-count, action, false);
+        }
         else while (hasPrevious() && count-- > 0) action.accept(previous());
     }
 
-    default void forEachPreviousUntil(long count, Consumer<? super E> action) {
-        forEachPreviousUntil(count, action, true);
+    /**
+     * Same functionality as {@link #forEachPrevious(java.util.function.Consumer)}, but stops early after a certian amount of iterations.
+     * Still checks {@link java.util.Iterator#hasPrevious()}; use {@link #forEachPreviousUntil_checkless(long, java.util.function.Consumer)} to not check that.
+     *
+     * @param count the maximum amount of iterations
+     * @param action the consumer that is run on all previous elements, in order
+     * @return the instantiating class, for chaining
+     */
+    default BiIterator<E> forEachPreviousUntil(long count, Consumer<? super E> action) {
+        __forEachPreviousUntil(count, action, true);
+        return this;
     }
 
-    default void forEachRemainingUntil(Function<? super E, Boolean> action) {
-        if (isReversed()) while (hasPrevious() && action.accept(previous()));
+    /**
+    * Same functionality as {@link #forEachNext(java.util.function.Consumer)}, but allows the function to control when it stops.
+    * Still checks {@link java.util.Iterator#hasNext()}; use {@link #forEachNextUntil_checkless(java.util.function.Function)} to not check that.
+    *
+    * @param action the function that accepts the remaining elements and returns `true` to keep going or `false` to stop
+    * @return the instantiating class, for chaining
+    */
+    default BiIterator<E> forEachNextUntil(Function<? super E, Boolean> action) {
+        if (isReversed()) while (hasPrevious() && action.apply(previous()));
         else while (hasNext() && action.accept(next()));
+        return this;
     }
 
-    default void forEachPreviousUntil(Function<? super E, Boolean> action) {
-        while (hasPrevious() && action.accept(previous()));
+    /**
+    * Same functionality as {@link #forEachPrevious(java.util.function.Consumer)}, but allows the function to control when it stops.
+    * Still checks {@link java.util.Iterator#hasPrevious()}; use {@link #forEachPreviousUntil_checkless(java.util.function.Function)} to not check that.
+    *
+    * @param action the function that accepts the previous elements and returns `true` to keep going or `false` to stop
+    * @return the instantiating class, for chaining
+    */
+    default BiIterator<E> forEachPreviousUntil(Function<? super E, Boolean> action) {
+        if (isReversed()) while (hasNext() && action.apply(next()));
+        else while (hasPrevious() && action.accept(previous()));
+        return this;
     }
 
     /**
     * Internal method call for the reversing implementation.&nbsp;Do not use.
-    * @see #forEachRemainingUntil(long, java.util.function.Function)
+    * @see #forEachNextUntil(long, java.util.function.Function)
     * @hidden
     *
-    * @param flag whether isReversed() should be ignored
+    * @param flag whether {@link #isReversed()} should be ignored
     */
-
-    default void forEachRemainingUntil(long count, Function<? super E, Boolean> action) {
-        if (count < 0) forEachPreviousUntil(-count, action);
+    default void __forEachNextUntil(long count, Function<? super E, Boolean> action, boolean flag) {
+        if (flag) {
+            if (isReversed()) count = -count;
+            if (count < 0) __forEachPreviousUntil(-count, action, false);
+        }
         else while (hasNext() && count-- > 0 && action.accept(next()));
+    }
+
+    default BiIterator<E> forEachNextUntil(long count, Function<? super E, Boolean> action) {
+        __forEachNextUntil(count, action, true);
+        return this;
     }
 
     /**
@@ -125,25 +216,41 @@ public interface BiIterator<E> extends ListIterator<E> {
     * @see #forEachPreviousUntil(long, java.util.function.Function)
     * @hidden
     *
-    * @param flag whether isReversed() should be ignored
+    * @param flag whether {@link #isReversed()} should be ignored
     */
 
-    default void forEachPreviousUntil(long count, Function<? super E, Boolean> action) {
-        if (count < 0) forEachRemainingUntil(-count, action);
+    default void __forEachPreviousUntil(long count, Function<? super E, Boolean> action, boolean flag) {
+        if (flag) {
+            if (isReversed()) count = -count;
+            if (count < 0) __forEachNextUntil(-count, action);
+        }
         else while (hasPrevious() && count-- > 0 && action.accept(previous()));
+    }
+
+    default BiIterator<E> forEachPreviousUntil(long count, Function<? super E, Boolean> action) {
+        __forEachPreviousUntil(count, action, true);
+        return this;
     }
 
     /**
     * Internal method call for the reversing implementation.&nbsp;Do not use.
-    * @see #forEachRemainingUntil_checkless(long, java.util.function.Consumer)
+    * @see #forEachNextUntil_checkless(long, java.util.function.Consumer)
     * @hidden
     *
-    * @param flag whether isReversed() should be ignored
+    * @param flag whether {@link #isReversed()} should be ignored
     */
 
-    default void forEachRemainingUntil_checkless(long count, Consumer<? super E> action) {
-        if (count < 0) forEachPreviousUntil(-count, action);
+    default void __forEachNextUntil_checkless(long count, Consumer<? super E> action, boolean flag) {
+        if (flag) {
+            if (isReversed()) count = -count;
+            if (count < 0) __forEachPreviousUntil_checkless(-count, action, false);
+        }
         else while (count-- > 0) action.accept(next());
+    }
+
+    default BiIterator<E> forEachNextUntil_checkless(long count, Consumer<? super E> action) {
+        __forEachNextUntil_checkless(count, action, true);
+        return this;
     }
 
     /**
@@ -151,15 +258,23 @@ public interface BiIterator<E> extends ListIterator<E> {
     * @see #forEachPreviousUntil_checkless(long, java.util.function.Consumer)
     * @hidden
     *
-    * @param flag whether isReversed() should be ignored
+    * @param flag whether {@link #isReversed()} should be ignored
     */
 
-    default void forEachPreviousUntil_checkless(long count, Consumer<? super E> action) {
-        if (count < 0) forEachRemainingUntil(-count, action);
+    default void __forEachPreviousUntil_checkless(long count, Consumer<? super E> action, boolean flag) {
+        if (flag) {
+            if (isReversed()) count = -count;
+            if (count < 0) __forEachNextUntil_checkless(-count, action, false);
+        }
         else while (count-- > 0) action.accept(previous());
     }
 
-    default void forEachRemainingUntil_checkless(Function<? super E, Boolean> action) {
+    default BiIterator<E> forEachPreviousUntil_checkless(long count, Consumer<? super E> action) {
+        __forEachPreviousUntil_checkless(count, action, true);
+        return this;
+    }
+
+    default void forEachNextUntil_checkless(Function<? super E, Boolean> action) {
         while (action.accept(next()));
     }
 
@@ -167,13 +282,13 @@ public interface BiIterator<E> extends ListIterator<E> {
         while (action.accept(previous()));
     }
 
-    default void forEachRemainingUntil_checkless(long count, Function<? super E, Boolean> action) {
+    default void forEachNextUntil_checkless(long count, Function<? super E, Boolean> action) {
         if (count < 0) forEachPreviousUntil(-count, action);
         else while (count-- > 0 && action.accept(next()));
     }
 
     default void forEachPreviousUntil_checkless(long count, Function<? super E, Boolean> action) {
-        if (count < 0) forEachRemainingUntil(-count, action);
+        if (count < 0) forEachNextUntil(-count, action);
         else while (count-- > 0 && action.accept(previous()));
     }
 
@@ -312,15 +427,17 @@ public interface BiIterator<E> extends ListIterator<E> {
     }
 
     default <T super E> T[] allNext(T[] arr) {
+        int len = arr.length;
         for (int i = 0;;) {
-            if (i == arr.length) return arr;
+            if (i == len) return arr;
             arr[i++] = next();
         }
     }
 
     default <T super E> T[] allPrevious(T[] arr) {
+        int len = arr.length;
         for (int i = 0;;) {
-            if (i == arr.length) return arr;
+            if (i == len) return arr;
             arr[i++] = previous();
         }
     }
