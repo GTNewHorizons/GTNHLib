3c5c8988e463e9102716d3b4b5463287dc96ffbf
__felix__ <134875831+felixfour@users.noreply.github.com>
2026-03-13 23:32:41 -0400

    create abstract 3d iterator

---
+++ src/main/java/com/gtnewhorizon/gtnhlib/geometry/Abstract3DIterator.java
@@ -0,0 +1,61 @@
+package com.gtnewhorizon.gtnhlib.geometry;
+
+import java.util.ListIterator;
+
+import com.gtnewhorizon.gtnhlib.blockpos.BlockPos;
+
+public abstract class Abstract3DIterator implements ListIterator<Void> {
+
+    public int n;
+    public int l;
+    public int m;
+
+    public abstract void __next();
+
+    public abstract void __previous();
+
+    public final Void next() {
+        __next();
+        return null;
+    }
+
+    public final Void previous() {
+        __previous();
+        return null;
+    }
+
+    public final void add(Void v) {}
+
+    public final void set(Void v) {}
+
+    public final void remove() {}
+
+    public int nextIndex() {
+        return 0;
+    }
+
+    public int previousIndex() {
+        return 0;
+    }
+
+    public final int[] nextCoordTriple() {
+        __next();
+        return new int[] { n , l , m };
+    }
+
+    public final BlockPos nextBlockPos() {
+        __next();
+        return new BlockPos(n, l, m);
+    }
+
+    public final int[] prevCoordTriple() {
+        __previous();
+        return new int[] { n , l , m };
+    }
+
+    public final BlockPos prevBlockPos() {
+        __previous();
+        return new BlockPos(n, l, m);
+    }
+
+}
