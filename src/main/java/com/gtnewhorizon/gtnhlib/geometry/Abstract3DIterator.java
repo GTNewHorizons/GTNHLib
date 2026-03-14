d910ec2df903fa6c73d03adabc639d27c5149663
__felix__ <134875831+felixfour@users.noreply.github.com>
2026-03-13 23:47:55 -0400

    spotless

--- src/main/java/com/gtnewhorizon/gtnhlib/geometry/Abstract3DIterator.java
+++ src/main/java/com/gtnewhorizon/gtnhlib/geometry/Abstract3DIterator.java
@@ -1,10 +1,10 @@
 package com.gtnewhorizon.gtnhlib.geometry;
 
-import java.util./*List*/Iterator;
+import java.util./* List */Iterator;
 
 import com.gtnewhorizon.gtnhlib.blockpos.BlockPos;
 
-public abstract class Abstract3DIterator implements /*List*/Iterator<Void> {
+public abstract class Abstract3DIterator implements /* List */Iterator<Void> {
 
     public int n;
     public int l;
@@ -18,35 +18,22 @@ public abstract class Abstract3DIterator implements /*List*/Iterator<Void> {
 
     public abstract void __next();
 
-    //public abstract void __previous();
+    // public abstract void __previous();
 
     public final Void next() {
         __next();
         return null;
     }
 
-    /*public final Void previous() {
-        __previous();
-        return null;
-    }
-
-    public final void add(Void v) {}
-
-    public final void set(Void v) {}
-
-    public final void remove() {}
-
-    public int nextIndex() {
-        return 0;
-    }
-
-    public int previousIndex() {
-        return 0;
-    }*/
+    /*
+     * public final Void previous() { __previous(); return null; } public final void add(Void v) {} public final void
+     * set(Void v) {} public final void remove() {} public int nextIndex() { return 0; } public int previousIndex() {
+     * return 0; }
+     */
 
     public final int[] nextCoordTriple() {
         __next();
-        return new int[] { n , l , m };
+        return new int[] { n, l, m };
     }
 
     public final BlockPos nextBlockPos() {
@@ -54,14 +41,9 @@ public abstract class Abstract3DIterator implements /*List*/Iterator<Void> {
         return new BlockPos(n, l, m);
     }
 
-    /*public final int[] prevCoordTriple() {
-        __previous();
-        return new int[] { n , l , m };
-    }
-
-    public final BlockPos prevBlockPos() {
-        __previous();
-        return new BlockPos(n, l, m);
-    }*/
+    /*
+     * public final int[] prevCoordTriple() { __previous(); return new int[] { n , l , m }; } public final BlockPos
+     * prevBlockPos() { __previous(); return new BlockPos(n, l, m); }
+     */
 
 }
