bdc843382098d4e9e4863f53247da2f9728e14f3
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2026-03-15 15:28:26 -0400

    clarify docs

--- src/main/java/com/gtnewhorizon/gtnhlib/api/IBlockModelProvider.java
+++ src/main/java/com/gtnewhorizon/gtnhlib/api/IBlockModelProvider.java
@@ -5,13 +5,9 @@ import net.minecraft.block.Block;
 import com.gtnewhorizon.gtnhlib.client.model.BakedModelQuadContext;
 import com.gtnewhorizon.gtnhlib.client.model.ModelISBRH;
 import com.gtnewhorizon.gtnhlib.client.model.baked.BakedModel;
-import com.gtnewhorizon.gtnhlib.client.model.loading.ModelRegistry;
 
-/**
- * An interface for {@link Block}s to allow providing custom models for {@link ModelISBRH}. Not necessary for standard
- * JSON models. This effectively allows for overwriting {@link ModelRegistry#getBakedModel} without needing a child
- * class, which avoids needing a new render ID (and associated mixins).
- */
+/// An interface for {@link Block}s to allow providing custom models for {@link ModelISBRH}. Not necessary for standard
+/// JSON models. This allows for custom model detection and baking logic.
 public interface IBlockModelProvider {
 
     BakedModel getModel(BakedModelQuadContext context);
