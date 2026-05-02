750aba08dadd45720902def123351588a038465e
Algent <algent@gmail.com>
2026-05-02 17:39:21 +0200

    javadoc

--- src/main/java/com/gtnewhorizon/gtnhlib/client/event/WorldDeletionEvent.java
+++ src/main/java/com/gtnewhorizon/gtnhlib/client/event/WorldDeletionEvent.java
@@ -4,9 +4,21 @@ import cpw.mods.fml.common.eventhandler.Event;
 
 public final class WorldDeletionEvent extends Event {
 
-    // Not a full path, name of just the directory as it is in .minecraft\saves\
     public final String worldName;
 
+    /**
+     * {@link WorldDeletionEvent} is posted on the {@link net.minecraftforge.common.MinecraftForge#EVENT_BUS} a world
+     * deletion is initiated through the world selection menu in the client and the world directory name has been
+     * resolved.
+     * </p>
+     * <p>
+     * {@link #worldName} String contains the name of the world directory as it exists under {@code .minecraft/saves},
+     * this is not a full or absolute filesystem path.
+     * </p>
+     * <p>
+     * This event is not {@link cpw.mods.fml.common.eventhandler.Cancelable}.
+     * </p>
+     */
     public WorldDeletionEvent(String worldName) {
         this.worldName = worldName;
     }
