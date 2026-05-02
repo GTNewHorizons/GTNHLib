87a4f6be161abc2434fb8ca770c51285087f942f
Algent <algent@gmail.com>
2026-05-02 16:16:59 +0200

    Requested changes

--- src/main/java/com/gtnewhorizon/gtnhlib/client/event/WorldDeletionEvent.java
+++ src/main/java/com/gtnewhorizon/gtnhlib/client/event/WorldDeletionEvent.java
@@ -1,12 +1,11 @@
 package com.gtnewhorizon.gtnhlib.client.event;
 
 import cpw.mods.fml.common.eventhandler.Event;
-import lombok.Getter;
 
 public final class WorldDeletionEvent extends Event {
 
-    @Getter
-    private final String worldName;
+    // Not a full path, name of just the directory as it is in .minecraft\saves\
+    public final String worldName;
 
     public WorldDeletionEvent(String worldName) {
         this.worldName = worldName;
