347f28817f5980b9ad44ec20e9b8dd3854b81023
FourIsTheNumber <33456283+FourIsTheNumber@users.noreply.github.com>
2026-05-06 11:27:06 -0400

    GTNHTeams: Arbitrary-data team system (#297)
    Co-authored-by: tiffit <10173431+tiffit@users.noreply.github.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Martin Robertz <dream-master@gmx.net>

---
+++ src/main/java/com/gtnewhorizon/gtnhlib/teams/TeamAdminCommand.java
@@ -0,0 +1,231 @@
+package com.gtnewhorizon.gtnhlib.teams;
+
+import static com.gtnewhorizon.gtnhlib.teams.TeamCommandsUtils.ARG_NEW_NAME;
+import static com.gtnewhorizon.gtnhlib.teams.TeamCommandsUtils.ARG_PLAYER;
+import static com.gtnewhorizon.gtnhlib.teams.TeamCommandsUtils.ARG_TEAM_NAME;
+import static com.gtnewhorizon.gtnhlib.teams.TeamCommandsUtils.resolveTeamMemberUuid;
+import static com.gtnewhorizon.gtnhlib.util.CommandUtils.argument;
+import static com.gtnewhorizon.gtnhlib.util.CommandUtils.colorChatComponent;
+import static com.gtnewhorizon.gtnhlib.util.CommandUtils.error;
+import static com.gtnewhorizon.gtnhlib.util.CommandUtils.literal;
+import static com.gtnewhorizon.gtnhlib.util.CommandUtils.success;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+
+import net.minecraft.command.ICommandSender;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.ChatComponentText;
+import net.minecraft.util.ChatComponentTranslation;
+import net.minecraft.util.EnumChatFormatting;
+
+import com.gtnewhorizon.gtnhlib.brigadier.BrigadierApi;
+import com.gtnewhorizon.gtnhlib.network.NetworkHandler;
+import com.gtnewhorizon.gtnhlib.network.teams.TeamInfoSync;
+import com.mojang.brigadier.Command;
+import com.mojang.brigadier.arguments.StringArgumentType;
+
+public class TeamAdminCommand {
+
+    public static void register() {
+        BrigadierApi.getCommandDispatcher().register(
+                literal("gtnhteam_admin").requires(src -> src.canCommandSenderUseCommand(2, "gtnhteam_admin"))
+                        .executes(ctx -> {
+                            sendUsage(ctx.getSource());
+                            return Command.SINGLE_SUCCESS;
+                        })
+                        .then(
+                                literal("rename").then(
+                                        argument(ARG_TEAM_NAME, StringArgumentType.word()).then(
+                                                argument(ARG_NEW_NAME, StringArgumentType.greedyString()).executes(
+                                                        ctx -> executeAdminRename(
+                                                                ctx.getSource(),
+                                                                StringArgumentType.getString(ctx, ARG_TEAM_NAME),
+                                                                StringArgumentType.getString(ctx, ARG_NEW_NAME))))))
+                        .then(
+                                literal("promote").then(
+                                        argument(ARG_TEAM_NAME, StringArgumentType.string()).then(
+                                                argument(ARG_PLAYER, StringArgumentType.string()).executes(
+                                                        ctx -> executeAdminPromote(
+                                                                ctx.getSource(),
+                                                                StringArgumentType.getString(ctx, ARG_TEAM_NAME),
+                                                                StringArgumentType.getString(ctx, ARG_PLAYER))))))
+                        .then(
+                                literal("demote").then(
+                                        argument(ARG_TEAM_NAME, StringArgumentType.string()).then(
+                                                argument(ARG_PLAYER, StringArgumentType.string()).executes(
+                                                        ctx -> executeAdminDemote(
+                                                                ctx.getSource(),
+                                                                StringArgumentType.getString(ctx, ARG_TEAM_NAME),
+                                                                StringArgumentType.getString(ctx, ARG_PLAYER))))))
+                        .then(
+                                literal("merge").then(
+                                        argument(ARG_TEAM_NAME, StringArgumentType.string()).then(
+                                                argument(ARG_TEAM_NAME, StringArgumentType.string()).executes(
+                                                        ctx -> executeAdminMerge(
+                                                                ctx.getSource(),
+                                                                StringArgumentType.getString(ctx, ARG_TEAM_NAME),
+                                                                StringArgumentType.getString(ctx, ARG_TEAM_NAME))))))
+                        .then(
+                                literal("disband").then(
+                                        argument(ARG_TEAM_NAME, StringArgumentType.greedyString()).executes(
+                                                ctx -> executeAdminDisband(
+                                                        ctx.getSource(),
+                                                        StringArgumentType.getString(ctx, ARG_TEAM_NAME)))))
+                        .then(
+                                literal("info").then(
+                                        argument(ARG_TEAM_NAME, StringArgumentType.greedyString()).executes(
+                                                ctx -> executeAdminInfo(
+                                                        ctx.getSource(),
+                                                        StringArgumentType.getString(ctx, ARG_TEAM_NAME)))))
+                        .then(literal("help").executes(ctx -> executeAdminHelp(ctx.getSource()))));
+    }
+
+    private static int executeAdminRename(ICommandSender sender, String oldName, String newName) {
+        Team team = TeamManager.getTeamByName(oldName);
+        if (team == null) return error(sender, "gtnhlib.chat.teams.admin.error.team_not_found", oldName);
+
+        if (!team.renameTeam(newName)) return error(sender, "gtnhlib.chat.teams.admin.error.name_in_use", newName);
+
+        TeamInfoSync packet = TeamNetwork.createTeamInfoSyncPacket(team);
+        TeamManager.forEachOnlineTeamMember(team, player -> NetworkHandler.instance.sendTo(packet, player));
+
+        return success(
+                sender,
+                "gtnhlib.chat.teams.admin.message.renamed",
+                colorChatComponent(EnumChatFormatting.GOLD, oldName),
+                colorChatComponent(EnumChatFormatting.GOLD, newName));
+    }
+
+    private static int executeAdminPromote(ICommandSender sender, String teamName, String playerName) {
+        Team team = TeamManager.getTeamByName(teamName);
+        if (team == null) return error(sender, "gtnhlib.chat.teams.admin.error.team_not_found", teamName);
+
+        UUID uuid = resolveTeamMemberUuid(team, playerName);
+        if (uuid == null || !team.isMember(uuid))
+            return error(sender, "gtnhlib.chat.teams.admin.error.player_not_in_team", playerName, teamName);
+        if (team.isOwner(uuid)) return error(sender, "gtnhlib.chat.teams.error.promote_owner", playerName);
+
+        ChatComponentText playerComp = colorChatComponent(EnumChatFormatting.GOLD, playerName);
+        ChatComponentText teamComp = colorChatComponent(EnumChatFormatting.GOLD, teamName);
+        if (team.isOfficer(uuid)) {
+            team.addOwner(uuid);
+            return success(sender, "gtnhlib.chat.teams.admin.message.promoted_to_owner", playerComp, teamComp);
+        } else {
+            team.addOfficer(uuid);
+            return success(sender, "gtnhlib.chat.teams.admin.message.promoted_to_officer", playerComp, teamComp);
+        }
+    }
+
+    private static int executeAdminDemote(ICommandSender sender, String teamName, String playerName) {
+        Team team = TeamManager.getTeamByName(teamName);
+        if (team == null) return error(sender, "gtnhlib.chat.teams.admin.error.team_not_found", teamName);
+
+        UUID uuid = resolveTeamMemberUuid(team, playerName);
+        if (uuid == null || !team.isMember(uuid))
+            return error(sender, "gtnhlib.chat.teams.admin.error.player_not_in_team", playerName, teamName);
+        if (!team.isOfficer(uuid)) return error(sender, "gtnhlib.chat.teams.error.demote_member", playerName);
+        if (team.isOwner(uuid) && team.getOwners().size() == 1)
+            return error(sender, "gtnhlib.chat.teams.admin.error.demote_last_owner", playerName, teamName);
+
+        ChatComponentText playerComp = colorChatComponent(EnumChatFormatting.GOLD, playerName);
+        ChatComponentText teamComp = colorChatComponent(EnumChatFormatting.GOLD, teamName);
+        if (team.isOwner(uuid)) {
+            team.removeOwner(uuid);
+            return success(sender, "gtnhlib.chat.teams.admin.message.demoted_to_officer", playerComp, teamComp);
+        } else {
+            team.removeOfficer(uuid);
+            return success(sender, "gtnhlib.chat.teams.admin.message.demoted_to_member", playerComp, teamComp);
+        }
+    }
+
+    private static int executeAdminMerge(ICommandSender sender, String sourceName, String targetName) {
+        Team sourceTeam = TeamManager.getTeamByName(sourceName);
+        if (sourceTeam == null) return error(sender, "gtnhlib.chat.teams.admin.error.team_not_found", sourceName);
+        Team targetTeam = TeamManager.getTeamByName(targetName);
+        if (targetTeam == null) return error(sender, "gtnhlib.chat.teams.admin.error.team_not_found", targetName);
+
+        List<UUID> allMembers = new ArrayList<>(sourceTeam.getMembers());
+        allMembers.addAll(targetTeam.getMembers());
+
+        TeamManager.mergeTeams(targetTeam, sourceTeam);
+
+        ChatComponentTranslation notification = new ChatComponentTranslation(
+                "gtnhlib.chat.teams.message.merge_complete",
+                colorChatComponent(EnumChatFormatting.GOLD, sourceName),
+                colorChatComponent(EnumChatFormatting.GOLD, targetName));
+        notification.getChatStyle().setColor(EnumChatFormatting.GREEN);
+
+        for (UUID memberUuid : allMembers) {
+            EntityPlayer member = sender.getEntityWorld().func_152378_a(memberUuid); // getPlayerByUUID
+            if (member != null) member.addChatMessage(notification);
+        }
+
+        return success(
+                sender,
+                "gtnhlib.chat.teams.admin.message.merged",
+                colorChatComponent(EnumChatFormatting.GOLD, sourceName),
+                colorChatComponent(EnumChatFormatting.GOLD, targetName));
+    }
+
+    private static int executeAdminDisband(ICommandSender sender, String teamName) {
+        Team team = TeamManager.getTeamByName(teamName);
+        if (team == null) return error(sender, "gtnhlib.chat.teams.admin.error.team_not_found", teamName);
+
+        List<UUID> members = new ArrayList<>(team.getMembers());
+
+        TeamManager.TEAMS.remove(team);
+        TeamManager.TEAM_MAP.remove(team.getTeamId());
+        TeamManager.PENDING_MERGE_REQUESTS.remove(team);
+        team.markRemoved();
+        for (Set<Team> teams : TeamManager.PENDING_INVITES.values()) {
+            teams.remove(team);
+        }
+
+        ChatComponentTranslation notice = new ChatComponentTranslation(
+                "gtnhlib.chat.teams.admin.message.team_disbanded",
+                colorChatComponent(EnumChatFormatting.GOLD, teamName));
+        notice.getChatStyle().setColor(EnumChatFormatting.RED);
+
+        for (UUID uuid : members) {
+            EntityPlayer member = sender.getEntityWorld().func_152378_a(uuid); // getPlayerByUUID
+            String name = member != null ? member.getCommandSenderName() : uuid.toString();
+            Team newTeam = TeamManager.getOrCreateTeam(name, uuid);
+            TeamManager.copyTeamData(team, newTeam, uuid, TeamDataCopyReason.JoinedNewTeam);
+            if (member != null) member.addChatMessage(notice);
+        }
+
+        return success(
+                sender,
+                "gtnhlib.chat.teams.admin.message.disbanded",
+                colorChatComponent(EnumChatFormatting.GOLD, teamName));
+    }
+
+    private static int executeAdminInfo(ICommandSender sender, String teamName) {
+        Team team = TeamManager.getTeamByName(teamName);
+        if (team == null) return error(sender, "gtnhlib.chat.teams.admin.error.team_not_found", teamName);
+
+        TeamCommandsUtils.printTeamInfo(sender, team);
+
+        return Command.SINGLE_SUCCESS;
+    }
+
+    private static int executeAdminHelp(ICommandSender sender) {
+        sender.addChatMessage(new ChatComponentTranslation("gtnhlib.chat.teams.admin.help.1"));
+        sender.addChatMessage(new ChatComponentTranslation("gtnhlib.chat.teams.admin.help.2"));
+        sender.addChatMessage(new ChatComponentTranslation("gtnhlib.chat.teams.admin.help.3"));
+        sender.addChatMessage(new ChatComponentTranslation("gtnhlib.chat.teams.admin.help.4"));
+        sender.addChatMessage(new ChatComponentTranslation("gtnhlib.chat.teams.admin.help.5"));
+        sender.addChatMessage(new ChatComponentTranslation("gtnhlib.chat.teams.admin.help.6"));
+        sender.addChatMessage(new ChatComponentTranslation("gtnhlib.chat.teams.admin.help.7"));
+        return Command.SINGLE_SUCCESS;
+    }
+
+    private static void sendUsage(ICommandSender sender) {
+        ChatComponentTranslation msg = new ChatComponentTranslation("gtnhlib.chat.teams.admin.message.usage");
+        msg.getChatStyle().setColor(EnumChatFormatting.YELLOW);
+        sender.addChatMessage(msg);
+    }
+}
---
+++ src/main/java/com/gtnewhorizon/gtnhlib/teams/TeamDataSaver.java
@@ -0,0 +1,220 @@
+package com.gtnewhorizon.gtnhlib.teams;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.Map;
+import java.util.UUID;
+
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
+import net.minecraft.nbt.NBTTagString;
+import net.minecraftforge.common.util.Constants;
+import net.minecraftforge.event.world.WorldEvent;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import com.gtnewhorizon.gtnhlib.GTNHLib;
+import com.gtnewhorizon.gtnhlib.eventbus.EventBusSubscriber;
+import com.gtnewhorizon.gtnhlib.util.NBTJson;
+
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.common.gameevent.PlayerEvent.PlayerLoggedInEvent;
+
+@EventBusSubscriber
+public class TeamDataSaver {
+
+    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+    private static final int SAVE_VER = 1;
+    public static TeamDataSaver INSTANCE;
+
+    private File saveDir;
+
+    private TeamDataSaver(File saveDir) {
+        this.saveDir = saveDir;
+        saveDir.mkdir();
+        loadFromFiles();
+    }
+
+    @SubscribeEvent
+    public static void onWorldLoad(WorldEvent.Load event) {
+        if (!event.world.isRemote && event.world.provider.dimensionId == 0) {
+            TeamManager.clear();
+            INSTANCE = new TeamDataSaver(new File(event.world.getSaveHandler().getWorldDirectory(), "gtnhteams"));
+        }
+    }
+
+    @SubscribeEvent
+    public static void onWorldUnload(WorldEvent.Unload event) {
+        if (!event.world.isRemote && event.world.provider.dimensionId == 0) {
+            if (INSTANCE != null) {
+                INSTANCE.saveToFiles();
+                TeamManager.clear();
+                INSTANCE = null;
+            }
+        }
+    }
+
+    @SubscribeEvent
+    public static void onWorldSave(WorldEvent.Save event) {
+        if (!event.world.isRemote && event.world.provider.dimensionId == 0) {
+            if (INSTANCE != null) {
+                INSTANCE.saveToFiles();
+            }
+        }
+    }
+
+    @SubscribeEvent
+    public static void onPlayerLogin(PlayerLoggedInEvent event) {
+        if (event.player instanceof EntityPlayerMP player) {
+            Team team = TeamManager.getOrCreateTeam(player.getCommandSenderName(), player.getUniqueID());
+            TeamNetwork.sendPlayerAllTeamData(player, team);
+        }
+    }
+
+    private void loadFromFiles() {
+        TeamManager.clear();
+        if (!saveDir.exists()) {
+            GTNHLib.error("Unable to load all teams, directory does not exist: " + saveDir);
+            return;
+        }
+        File[] files = saveDir.listFiles((dir, name) -> name.endsWith(".json"));
+        if (files == null) return;
+        for (File file : files) {
+            if (file.isFile()) {
+                try (FileReader reader = new FileReader(file)) {
+                    JsonObject obj = GSON.fromJson(reader, JsonObject.class);
+                    Team team = loadFromNBT((NBTTagCompound) NBTJson.toNbt(obj));
+                    TeamManager.addTeamDeduplicated(team);
+                } catch (Exception e) {
+                    GTNHLib.LOG.error("Unable to load team {}", file.getName(), e);
+                }
+            }
+        }
+    }
+
+    private Team loadFromNBT(NBTTagCompound teamTag) {
+        String teamName = teamTag.getString("TeamName");
+        UUID uuid = UUID.fromString(teamTag.getString("UUID"));
+
+        Team team = new Team(teamName, uuid);
+
+        // Owners
+        NBTTagList ownersList = teamTag.getTagList("Owners", Constants.NBT.TAG_STRING);
+        for (int j = 0; j < ownersList.tagCount(); j++) {
+            team.addOwner(UUID.fromString(ownersList.getStringTagAt(j)));
+        }
+
+        // Officers
+        NBTTagList officersList = teamTag.getTagList("Officers", Constants.NBT.TAG_STRING);
+        for (int j = 0; j < officersList.tagCount(); j++) {
+            team.addOfficer(UUID.fromString(officersList.getStringTagAt(j)));
+        }
+
+        // Members
+        NBTTagList membersList = teamTag.getTagList("Members", Constants.NBT.TAG_STRING);
+        for (int j = 0; j < membersList.tagCount(); j++) {
+            team.addMember(UUID.fromString(membersList.getStringTagAt(j)));
+        }
+
+        NBTTagCompound teamData = teamTag.getCompoundTag("TeamData");
+        for (String key : TeamDataRegistry.getRegisteredKeys()) {
+            try {
+                ITeamData data = TeamDataRegistry.construct(key);
+                if (data != null && teamData.hasKey(key)) {
+                    data.readFromNBT(teamData.getCompoundTag(key));
+                    team.putData(key, data);
+                }
+            } catch (Exception ex) {
+                GTNHLib.LOG.error("Error while loading TeamData {} for team {}", key, uuid, ex);
+            }
+        }
+        return team;
+    }
+
+    private void saveToFiles() {
+        if (!saveDir.exists() && !saveDir.mkdir()) {
+            GTNHLib.error("Unable to save all teams, unable to create directory: " + saveDir);
+            return;
+        }
+
+        for (Team team : TeamManager.TEAMS) {
+            TeamSaveStatus status = team.getStatus();
+            if (status == TeamSaveStatus.CLEAN) continue;
+            String fileName = team.getTeamId().toString() + ".json";
+            if (status == TeamSaveStatus.DIRTY) {
+                try {
+                    File saveFile = new File(saveDir, fileName);
+                    NBTTagCompound tag = writeToNBT(team);
+                    String json = GSON.toJson(NBTJson.toJsonObject(tag));
+                    Files.write(saveFile.toPath(), json.getBytes(StandardCharsets.UTF_8));
+                    team.markClean();
+                } catch (Exception e) {
+                    GTNHLib.LOG.error("Unable to save team {} ({})", team.getTeamId(), team.getTeamName(), e);
+                }
+            }
+        }
+
+        for (UUID removedTeam : TeamManager.REMOVED_TEAMS) {
+            String fileName = removedTeam + ".json";
+            File deleteFile = new File(saveDir, fileName);
+            try {
+                Files.deleteIfExists(deleteFile.toPath());
+            } catch (IOException e) {
+                GTNHLib.LOG.error("Unable to delete team {}", fileName, e);
+            }
+        }
+        TeamManager.REMOVED_TEAMS.clear();
+    }
+
+    private NBTTagCompound writeToNBT(Team team) {
+        NBTTagCompound teamTag = new NBTTagCompound();
+        teamTag.setInteger("Version", SAVE_VER);
+        teamTag.setString("TeamName", team.getTeamName());
+        teamTag.setString("UUID", team.getTeamId().toString());
+
+        // Owners
+        NBTTagList ownersList = new NBTTagList();
+        for (UUID owner : team.getOwners()) {
+            ownersList.appendTag(new NBTTagString(owner.toString()));
+        }
+        teamTag.setTag("Owners", ownersList);
+
+        // Officers
+        NBTTagList officersList = new NBTTagList();
+        for (UUID officer : team.getOfficers()) {
+            officersList.appendTag(new NBTTagString(officer.toString()));
+        }
+        teamTag.setTag("Officers", officersList);
+
+        // Members
+        NBTTagList membersList = new NBTTagList();
+        for (UUID member : team.getMembers()) {
+            membersList.appendTag(new NBTTagString(member.toString()));
+        }
+        teamTag.setTag("Members", membersList);
+
+        // Team Data
+        NBTTagCompound dataTag = new NBTTagCompound();
+        for (Map.Entry<String, ITeamData> entry : team.getAllDataEntries()) {
+            try {
+                NBTTagCompound entryTag = new NBTTagCompound();
+                entry.getValue().writeToNBT(entryTag);
+                dataTag.setTag(entry.getKey(), entryTag);
+            } catch (Exception ex) {
+                GTNHLib.LOG.error(
+                        "Error while saving TeamData {} for team {}",
+                        entry.getKey(),
+                        team.getTeamId().toString(),
+                        ex);
+            }
+        }
+        teamTag.setTag("TeamData", dataTag);
+
+        return teamTag;
+    }
+}
