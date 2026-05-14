35ef6c48d2c5872f52b2820740e330b7558598de
cubefury <thetarragondevteam@gmail.com>
2026-05-15 03:04:47 +0800

    Remove debug messages

--- src/main/java/com/gtnewhorizon/gtnhlib/network/PlayerDataSyncHandler.java
+++ src/main/java/com/gtnewhorizon/gtnhlib/network/PlayerDataSyncHandler.java
@@ -1,6 +1,5 @@
 package com.gtnewhorizon.gtnhlib.network;
 
-import com.gtnewhorizon.gtnhlib.GTNHLib;
 import com.gtnewhorizon.gtnhlib.util.ClientPlayerUtils;
 
 import cpw.mods.fml.common.network.simpleimpl.IMessage;
@@ -11,8 +10,6 @@ public class PlayerDataSyncHandler implements IMessageHandler<PlayerDataSync, IM
 
     @Override
     public IMessage onMessage(PlayerDataSync message, MessageContext ctx) {
-        GTNHLib.LOG.info("sync arrived with length {}", message.data.size());
-        message.data.forEach((name, id) -> GTNHLib.LOG.info("{} {}", name, id));
         synchronized (ClientPlayerUtils.clientPlayerMap) {
             ClientPlayerUtils.clientPlayerMap.clear();
             ClientPlayerUtils.clientPlayerMap.putAll(message.data);
