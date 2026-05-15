174f28b179c48e35d42700354d8f43a679c26deb
cubefury <thetarragondevteam@gmail.com>
2026-05-16 00:24:40 +0800

    Include more data client-side for UI display

--- src/main/java/com/gtnewhorizon/gtnhlib/eventhandlers/ServerEventHandler.java
+++ src/main/java/com/gtnewhorizon/gtnhlib/eventhandlers/ServerEventHandler.java
@@ -1,14 +1,14 @@
 package com.gtnewhorizon.gtnhlib.eventhandlers;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
-
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraftforge.common.UsernameCache;
 
 import com.gtnewhorizon.gtnhlib.eventbus.EventBusSubscriber;
 import com.gtnewhorizon.gtnhlib.network.NetworkHandler;
 import com.gtnewhorizon.gtnhlib.network.PlayerDataSync;
+import com.gtnewhorizon.gtnhlib.teams.Team;
+import com.gtnewhorizon.gtnhlib.teams.TeamManager;
+import com.gtnewhorizon.gtnhlib.teams.TeamNetwork;
 import com.gtnewhorizon.gtnhlib.util.ServerPlayerUtils;
 
 import cpw.mods.fml.common.eventhandler.EventPriority;
@@ -20,14 +20,13 @@ public class ServerEventHandler {
 
     @SubscribeEvent(priority = EventPriority.LOW)
     public static void onPlayerLogin(PlayerEvent.PlayerLoggedInEvent event) {
-        Map<String, UUID> newMap = new HashMap<>();
-        UsernameCache.getMap().forEach((uuid, name) -> { newMap.put(name, uuid); });
-        synchronized (ServerPlayerUtils.serverPlayerMap) {
-            ServerPlayerUtils.serverPlayerMap.clear();
-            ServerPlayerUtils.serverPlayerMap.putAll(newMap);
-        }
         PlayerDataSync sync = new PlayerDataSync();
-        sync.data.putAll(newMap);
+        sync.data.addAll(UsernameCache.getMap().values());
         ServerPlayerUtils.forAllOnlinePlayers(player -> NetworkHandler.instance.sendTo(sync, player));
+
+        if (event.player instanceof EntityPlayerMP player) {
+            Team team = TeamManager.getOrCreateTeam(player.getCommandSenderName(), player.getUniqueID());
+            TeamNetwork.sendPlayerAllTeamData(player, team);
+        }
     }
 }
--- src/main/java/com/gtnewhorizon/gtnhlib/network/PlayerDataSync.java
+++ src/main/java/com/gtnewhorizon/gtnhlib/network/PlayerDataSync.java
@@ -1,8 +1,7 @@
 package com.gtnewhorizon.gtnhlib.network;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
+import java.util.HashSet;
+import java.util.Set;
 
 import cpw.mods.fml.common.network.ByteBufUtils;
 import cpw.mods.fml.common.network.simpleimpl.IMessage;
@@ -10,25 +9,21 @@ import io.netty.buffer.ByteBuf;
 
 public class PlayerDataSync implements IMessage {
 
-    public Map<String, UUID> data = new HashMap<>();
+    public Set<String> data = new HashSet<>();
 
     public PlayerDataSync() {}
 
     @Override
     public void toBytes(ByteBuf buf) {
         buf.writeInt(data.size());
-        data.forEach((name, uuid) -> {
-            ByteBufUtils.writeUTF8String(buf, name);
-            buf.writeLong(uuid.getMostSignificantBits());
-            buf.writeLong(uuid.getLeastSignificantBits());
-        });
+        data.forEach(name -> ByteBufUtils.writeUTF8String(buf, name));
     }
 
     @Override
     public void fromBytes(ByteBuf buf) {
         int length = buf.readInt();
         for (int i = 0; i < length; i++) {
-            data.put(ByteBufUtils.readUTF8String(buf), new UUID(buf.readLong(), buf.readLong()));
+            data.add(ByteBufUtils.readUTF8String(buf));
         }
     }
 
