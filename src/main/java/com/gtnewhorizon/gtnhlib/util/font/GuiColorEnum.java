1326b54106febe5adbe35590458c0ea7607a0b4b
alppp <turcu.dan991@gmail.com>
2026-05-25 18:15:18 +0300

    add GuiColorEnum interface for mod GuiColors enums

--- src/main/java/com/gtnewhorizon/gtnhlib/util/font/GuiColorEnum.java
+++ src/main/java/com/gtnewhorizon/gtnhlib/util/font/GuiColorEnum.java
@@ -1,13 +1,17 @@
 package com.gtnewhorizon.gtnhlib.util.font;
 
+import net.minecraft.client.gui.FontRenderer;
+
 /**
  * Interface for mod GuiColors enums. Provides format code support from lang files via
- * {@link FontRendering#getTextPrefix(String)}. Mods implement {@link #getUnlocalized()} and get {@link #getTextPrefix()}
- * and {@link #format(String)} for free.
+ * {@link FontRendering#getTextPrefix(String)}. Mods implement {@link #getUnlocalized()} and {@link #getColor()}, then
+ * get {@link #format(String)} and {@link #drawString(FontRenderer, String, int, int)} for free.
  */
 public interface GuiColorEnum {
 
     String getUnlocalized();
+
+    int getColor();
 
     default String getTextPrefix() {
         return FontRendering.getTextPrefix(getUnlocalized());
@@ -15,5 +19,9 @@ public interface GuiColorEnum {
 
     default String format(String text) {
         return getTextPrefix() + text;
+    }
+
+    default int drawString(FontRenderer fr, String text, int x, int y) {
+        return fr.drawString(format(text), x, y, getColor());
     }
 }
