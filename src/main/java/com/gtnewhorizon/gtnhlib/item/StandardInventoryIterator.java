2b024fade418743224fbec3d02dc95f6f0a55588
attempt0 <nanolify@gmail.com>
2026-06-07 23:13:09 +0800

    Avoid NBT matching (#378)
    Co-authored-by: Martin Robertz <dream-master@gmx.net>

--- src/main/java/com/gtnewhorizon/gtnhlib/item/StandardInventoryIterator.java
+++ src/main/java/com/gtnewhorizon/gtnhlib/item/StandardInventoryIterator.java
@@ -107,17 +107,25 @@ public class StandardInventoryIterator extends AbstractInventoryIterator {
 
         ItemStack inSlot = getStackInSlot(slotIndex);
 
-        if (!ItemUtil.isStackEmpty(inSlot) && !stack.matches(inSlot)) {
-            return stack.getStackSize();
-        }
-
         ItemStack partialCopy = stack.toStackFast();
+
+        int maxStack = getSlotStackLimit(slotIndex, partialCopy);
+
+        if (!ItemUtil.isStackEmpty(inSlot)) {
+            if (!forced) {
+                // Skip expensive NBT check if the stack is full.
+                if (inSlot.stackSize >= maxStack || !inSlot.isStackable()) {
+                    return stack.getStackSize();
+                }
+            }
+            if (!stack.matches(inSlot)) {
+                return stack.getStackSize();
+            }
+        }
 
         if (!forced && !canInsert(partialCopy, slotIndex)) {
             return stack.getStackSize();
         }
-
-        int maxStack = getSlotStackLimit(slotIndex, partialCopy);
 
         if (!ItemUtil.isStackEmpty(inSlot)) {
             int toInsert = forced ? stack.getStackSize() : Math.min(maxStack - inSlot.stackSize, stack.getStackSize());
