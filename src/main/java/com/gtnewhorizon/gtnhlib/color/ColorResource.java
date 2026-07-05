f87e6e15281d52257b4f9c54782fe784d30e85ac
Ranzu <66495944+Ranzuu@users.noreply.github.com>
2026-07-05 15:58:10 +0200

    space

--- src/main/java/com/gtnewhorizon/gtnhlib/color/ColorResource.java
+++ src/main/java/com/gtnewhorizon/gtnhlib/color/ColorResource.java
@@ -179,7 +179,6 @@ public class ColorResource {
             synchronized (INSTANCES) {
                 for (ColorResource instance : INSTANCES) {
                     instance.cachedColor = instance.resolveColor();
-
                 }
             }
         }
