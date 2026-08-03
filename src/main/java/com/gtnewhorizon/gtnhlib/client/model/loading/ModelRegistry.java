d337977e25cdea1b934845d0a69b9068ef796f67
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2026-08-03 10:05:08 -0400

    Use a set for gathering textures

--- src/main/java/com/gtnewhorizon/gtnhlib/client/model/loading/ModelRegistry.java
+++ src/main/java/com/gtnewhorizon/gtnhlib/client/model/loading/ModelRegistry.java
@@ -4,7 +4,8 @@ import static com.gtnewhorizon.gtnhlib.GTNHLibConfig.modelCacheSize;
 import static com.gtnewhorizon.gtnhlib.client.model.unbaked.MissingModel.MISSING_MODEL;
 import static com.gtnewhorizon.gtnhlib.core.GTNHLibCore.MODEL_LOGGER;
 
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 import net.minecraft.block.Block;
 import net.minecraft.client.resources.FallbackResourceManager;
@@ -36,8 +37,6 @@ import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.registry.GameData;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
-import it.unimi.dsi.fastutil.objects.ObjectArrayList;
-import it.unimi.dsi.fastutil.objects.ObjectLists;
 import it.unimi.dsi.fastutil.objects.ObjectOpenHashSet;
 
 /// Handles model loading and caching. All caches are size-based - this means that if a model has enough parents, it may
@@ -170,7 +169,7 @@ public class ModelRegistry {
 
             // Gather the list of modeled blocks and their textures
             final var modeledBlocks = new ObjectOpenHashSet<String>();
-            final var texturesToLoad = new ObjectArrayList<String>();
+            final var texturesToLoad = new HashSet<String>();
             for (var pack : resourcePacks) {
                 if (!(pack instanceof ModelResourcePack mrp)) continue;
 
@@ -184,6 +183,8 @@ public class ModelRegistry {
                 texturesToLoad.addAll(info.textureNames());
             }
 
+            // Sadly, this field is untyped and we can't do anything about it... not easily, anyway.
+            // noinspection unchecked
             GameData.getBlockRegistry().registryObjects.forEach((s, b) -> {
                 if (!(s instanceof String name)) return;
                 if (!(b instanceof Block block)) return;
@@ -216,7 +217,7 @@ public class ModelRegistry {
 
     public static class EventHandler {
 
-        private static @NotNull List<String> texturesToLoad = ObjectLists.emptyList();
+        private static @NotNull Set<String> texturesToLoad = Set.of();
 
         @SubscribeEvent
         @SideOnly(Side.CLIENT)
@@ -224,7 +225,7 @@ public class ModelRegistry {
             for (var texture : texturesToLoad) {
                 TexHelper.registerTexture(event.map, texture);
             }
-            texturesToLoad = ObjectLists.emptyList(); // don't need it anymore
+            texturesToLoad = Set.of(); // don't need it anymore
         }
     }
 }
